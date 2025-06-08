c3ba69af6e80026db8d93c0cc64aaa972e3ddb9a
Lee Dong Hyun <aopo3948@gmail.com>
2025-06-08 22:15:55 +0900

    2002 추월
    1.
    나가는 번호가 들어온 번호보다 빠를 경우 추월함
    기존에 앞서가던 차량의 순서에 + 1 을 하기
    
    2.
    나가는 순서대로 차량을 확인
    뒤에 있는 차량들을 전부 검사해서 현재 차량이
    들어온 순서가 높을 경우 추월했다고 판정

---
+++ Baekjoon/2002 추월.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int num{}, count{}, curr{};
+	string car{};
+	map<string, int> car_list;
+
+	cin >> num;
+	for (int i = 0; i < num; ++i) {
+		cin >> car;
+		car_list[car] = i;
+	}
+
+	for (int j = 0; j < num; ++j) {
+		cin >> car;
+		curr = car_list[car];
+		if (curr > j) {								// 추월 했다면
+			++count;
+
+			for (auto& [name, order] : car_list) {	// 기존에 앞에 있던 차량들 순서 + 1
+				if (order < curr) {
+					++order;
+				}
+			}
+		}
+	}
+
+	cout << count << '\n';
+}
+
+//int main()
+//{
+//	int num{}, count{};
+//	string car{};
+//	map<string, int> input_list;
+//	vector<int> output_list;
+//
+//	cin >> num;
+//	for (int i = 0; i < num; ++i) {
+//		cin >> car;
+//		input_list[car] = i;
+//	}
+//	
+//	for (int i = 0; i < num; ++i) {						// 나오는 순서만 저장
+//		cin >> car;
+//		output_list.push_back(input_list[car]);
+//	}
+//
+//	for (int i = 0; i < num - 1; ++i) {					// 나오는 순서를 검사했을 때
+//		for (int j = i + 1; j < num; ++j) {				// 뒤에 있는 것 보다 숫자가 높으면 추월
+//			if (output_list[i] > output_list[j]) {
+//				++count;
+//				break;
+//			}
+//		}
+//	}
+//
+//
+//	cout << count << '\n';
+//}
