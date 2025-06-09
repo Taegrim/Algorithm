7875534c4aeaad3361e853b82a656f9efda61b20
Lee Dong Hyun <aopo3948@gmail.com>
2025-06-09 22:13:44 +0900

    9093 단어 뒤집기
    istringstream 과 getline 을 이용하여 문장을 단어로 분리
    1. stack 을 이용하여 문제를 해결
    2. 문자의 순서를 직접 바꿔서 문제를 해결
    
    파일 정리

---
+++ Baekjoon/9093 단어 뒤집기.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stack>
+
+using namespace std;
+
+// --------------------  Stack 사용 X ---------------
+
+int main()
+{
+	int n{}, num{};
+	string line{}, word{};
+	char temp{};
+
+	cin >> n;
+	cin.ignore();
+
+	for (int i = 0; i < n; ++i) {
+		getline(cin, line);
+
+		istringstream iss(line);
+
+		while (getline(iss, word, ' ')) {
+			num = word.size();
+
+			for (int j = 0; j < (num / 2); ++j) {
+				temp = word[j];
+				word[j] = word[num - j - 1];
+				word[num - j - 1] = temp;
+			}
+			cout << word << " ";
+		}
+		cout << '\n';
+	}
+}
+
+//// --------------------  Stack 사용 O ---------------
+//
+//int main()
+//{
+//	int n{};
+//	string line{};
+//	stack<char> words{};
+//
+//	cin >> n;
+//	cin.ignore();
+//
+//	for (int i = 0; i < n; ++i) {
+//		getline(cin, line);
+//		line += ' ';
+//
+//		for (const char c : line) {
+//			if (c == ' ') {				// 단어의 끝을 만나면
+//				while (!words.empty()) {
+//					cout << words.top();
+//					words.pop();
+//				}
+//				cout << " ";
+//			}
+//			else {
+//				words.push(c);
+//			}
+//		}
+//		cout << '\n';
+//	}
+//}
