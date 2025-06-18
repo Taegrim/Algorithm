91c914d43d5160ec39f051c0f8f7744b1432cf10
Lee Dong Hyun <aopo3948@gmail.com>
2025-06-18 23:31:20 +0900

    9935 문자열 폭발
    find, erase 를 사용하였더니 시간 초과가 나옴
     - 찾아서 지웠다면 다시 처음부터 찾는 것을 반복하기 때문
    -> 앞에서부터 검사하면서 일치하는 것을 지웠다면 이후부터 이어가도록 해야함
    
    앞에서부터 문자를 확인하면서 다른 문자열에 추가함
    추가하려는 문자가 폭탄의 마지막 글자와 같다면 그때 일치하는지 검사함
     - string 의 substr를 사용해서 검사함
    
    맞다면 string 의 erase 를 사용해서 지우고 다시 문자를 추가함

---
+++ Baekjoon/9935 문자열 폭발.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string s{}, bomb{};
+	string result{}, temp{};
+	int bomb_size{};
+
+	cin >> s >> bomb;
+
+	bomb_size = bomb.size();
+
+	for (const auto& c : s) {
+		result += c;
+
+		if (c == *(bomb.end() - 1) && result.size() >= bomb.size()) {
+			temp = result.substr(result.size() - bomb_size);
+
+			if (temp == bomb) {
+				result.erase(result.size() - bomb_size);
+			}
+		}
+	}
+
+	if (result.size() == 0) {
+		result = "FRULA";
+	}
+
+	cout << result << "\n";
+}
+
+
+//
+// 기존 find - replace 사용 시 시간 초과
+//  -> 찾아서 지우면 다시 처음부터 검사하기 때문
+//     지운 이후엔 그 뒤부터 검사할 수 있어야 함
+// 
+// 문자열을 result 에 앞에서부터 추가
+// 추가하려는 문자가 폭탄의 끝 글자와 같으면 그 때 result 의 뒤에서부터 일치하는지 검사
+//  -> string 의 substr 을 사용
+// 
+// 맞다면 string 의 erase 를 사용해서 지우고 추가하면서 위를 반복
+// 
+//
