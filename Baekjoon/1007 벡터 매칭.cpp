e02f67d5dea2ddf82ab7830b8f068b5d0b1ec817
Lee Dong Hyun <aopo3948@gmail.com>
2025-06-27 18:00:58 +0900

    1007 벡터 매칭
    벡터는 끝점 - 시작점이므로
    벡터의 합은 (끝점1 - 시작점1) + (끝점2 - 시작점2) + ... 이 됨
    이를 다시 풀어보면 (끝점1 + 끝점2 +...) - (시작점1 + 시작점2 + ...) 가 됨
    따라서 끝점들의 합에서 시작점의 합을 빼면 벡터의 합이고 이 길이를 구하면 됨
    
    벡터 집합 P 에서 절반은 시작점, 절반은 끝점이므로 이를 분리해야 함
     -> nC(n/2) 조합으로 정점을 분리할 수 있음
    
    mask - prev_permutation 으로 nCr 조합을 구할 수 있고
    마스킹 된 정점을 끝점 마스킹 되지 않은 정점을 시작점으로 볼 것임
    
    이후에 정점들의 합으로 벡터 길이를 구하고 최솟값을 구하여 해결함

---
+++ Baekjoon/1007 벡터 매칭.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int t{}, n{}, x{}, y{};
+	long long start_x{}, start_y{}, end_x{}, end_y{};
+	double result{ numeric_limits<double>::max() }, dist{};
+	vector<pair<int, int>> points{};
+	vector<bool> mask{};
+
+	cin >> t;
+
+	for (int i = 0; i < t; ++i) {
+		cin >> n;
+
+		for (int j = 0; j < n; ++j) {
+			cin >> x >> y;
+			points.emplace_back(make_pair(x, y));
+			mask.emplace_back(false);
+		}
+
+		for (int j = 0; j < n / 2; ++j) {		// 절반만 true로, true 인 경우 끝점, false면 시작점
+			mask[j] = true;
+		}
+		
+		do {
+			start_x = start_y = end_x = end_y = 0;
+
+			for (int j = 0; j < n; ++j) {
+				if (mask[j]) {		// 끝점
+					end_x += points[j].first;
+					end_y += points[j].second;
+				}
+				else {				// 시작점
+					start_x += points[j].first;
+					start_y += points[j].second;
+				}
+			}
+
+			end_x -= start_x;		// (끝점의 합) - (시작점의 합)
+			end_y -= start_y;
+			dist = sqrt(end_x * end_x + end_y * end_y);
+			result = min(dist, result);
+
+		} while (prev_permutation(mask.begin(), mask.end()));
+
+
+		cout << fixed;
+		cout.precision(12);
+		cout << result << "\n";
+
+		// 테스트 케이스 반복을 위한 초기화
+		points.clear();	
+		mask.clear();
+		result = numeric_limits<double>::max();
+	}
+}
+
+
+
+// (10,10) + (-10, 10) = (0,20)
+//
+// (-5,5) - (5, 5)   = (-10, 0)
+// (5, -5) - (-5,-5) = (10, 0)
+//   -> (0,0) -> sqrt(0^2 + 0^2)
+// 
+// (끝점1) - (시작점1) + (끝점2) - (시작점2) + ...
+// ((끝점1) + (끝점2) + ...) - ((시작점1) + (시작점2) + ...) 
+// 
+// 집합P 에서 절반은 시작점 절반은 끝점이 됨
+// 이를 반씩 분리할 수 있어야 함
+// 
+// nC(n/2) 조합으로 정점들을 분리해야 함
+//  -> mask, prev_permutation 으로 nCr 처리
+//
