f50482be440183943e070dfa6ab9e74ace873b7d
LeeDongHyun <aopo3948@gmail.com>
2026-04-20 09:45:45 +0900

    롤케이크 자르기
    맨 처음 인덱스를 기준으로 양쪽을 unordered_set을 만드는 방식으로 풀이 해봄
     - 시간 초과로 실패
    
    -> 매번 나누는게 아니라 오른쪽으로 갈때마다 왼쪽에 하나씩만 추가해야 함
    
    오른쪽 정보를 미리 초기화 하고, 탐색할때마다 왼쪽에 추가하는 방식으로 풀이로 통과
    왼쪽은 종류만 알아도 되므로 unordered_set을 이용하면 추가로 메모리 절약 가능

---
+++ Algorithm/Programmers/롤케이크 자르기.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+
+// 동일한 가짓수의 토핑 수로 나누기
+
+//// 매번 양쪽을 나누고 set으로 체크할 경우 시간초과!
+//int solution(vector<int> topping) {
+//    int answer = 0;
+//
+//    for (int i = 0; i < topping.size(); ++i) {
+//        unordered_set<int> left(topping.begin(), topping.begin() + i);
+//        unordered_set<int> right(topping.begin() + i, topping.end());
+//
+//        if (left.size() == right.size()) {
+//            ++answer;
+//        }
+//    }
+//
+//    return answer;
+//}
+
+// 양쪽을 동시에 체크하면?
+//  -> 오른쪽 방향으로 순회하면서 오른쪽 정보를 알아야함
+//     순회하면서 몇종류인지 체크, 남은게 몇종류인지 체크
+
+int solution(vector<int> topping) {
+    int answer = 0;
+
+    // 왼쪽, 오른쪽 토핑의 종류와 개수 unordered_map
+    // 왼쪽은 종류만 알아도 되므로 unordered_set으로 해도 됨
+    unordered_map<int, int> left, right;
+
+    // 탐색 전 남은 종류 초기화
+    for (int i : topping) {
+        ++right[i];
+    }
+
+    for (int i : topping) {
+        // 왼쪽에 추가
+        ++left[i];
+
+        // 오른쪽 남은 토핑에서 제거
+        --right[i];
+        if (right[i] == 0) {
+            right.erase(i);
+        }
+
+        if (left.size() == right.size()) {
+            ++answer;
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution({ 1, 2, 1, 3, 1, 4, 1, 2 });
+    solution({ 1, 2, 3, 1, 4 });
+}
