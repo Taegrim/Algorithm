c8e0c9ace7abce483e03c9ad5e86f7327d516e63
LeeDongHyun <aopo3948@gmail.com>
2026-04-30 11:56:45 +0900

    택배 상자, 큰 수 만들기
    택배 상자
     - 문제에서 보관용 컨테이너가 스택이라고 설명되어있으니 이를 사용
     - 큐와 스택을 확인하면서 시뮬레이션하여 풀이함
     - 기존 레일의 박스를 항상 스택에 넣고 보조 레일에서 적재를 수행하는 방식으로도 풀이함
    
    큰 수 만들기
     - 앞에서부터 확인해서 큰 수를 만나면 이전 값들을 확인해서 제거하는 방식
     - string 을 스택으로 보고 문자열을 조합하면서 뒤의 값보다 큰 문자를 만나면 제거함
     - 이후 문자열이 완성하고도 잘라내야한다면 뒤에서 제거

---
+++ Algorithm/Programmers/큰 수 만들기.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+// 앞에서부터 스택에 담기
+// 스택의 위보다 큰 수를 만나면 스택의 위부터 제거
+
+string solution(string number, int k) {
+    string answer = "";
+
+    for (char c : number) {
+        // 잘라낼 횟수가 남아있고
+        // 조합한 문자열의 뒤에서부터 확인해서 큰 문자를 만나면 제거
+        while (!answer.empty() && k > 0 && answer.back() < c) {
+            answer.pop_back();
+            --k;
+        }
+        answer.push_back(c);
+    }
+
+    // 문자열 완성되고도 잘라낼 횟수가 남았다면 뒤에서 제거
+    while (k > 0) {
+        answer.pop_back();
+        --k;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution("1924", 2);
+    solution("1231234", 2);
+    solution("4177252841", 2);
+}
---
+++ Algorithm/Programmers/택배 상자.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+
+
+#include <vector>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+// 큐 순서가 1,2,3,4,5 이므로 만들지 않고 box 라는 int 로 관리
+int solution(vector<int> order)
+{
+    stack<int> other;
+    int idx = 0;    // 현재 박스 순서, 적재한 박스 개수
+
+    // 현재 기본 레일의 맨 앞부터 끝까지 loop 돌리기만 함
+    // 항상 보조레일에 넣고 보조레일로 검사
+    for (int box = 1; box <= order.size(); ++box) {
+        other.push(box);
+
+        // 보조 레일에서만 트럭에 적재 수행
+        while (!other.empty() && other.top() == order[idx]) {
+            other.pop();
+            ++idx;
+        }
+    }
+
+    return idx;
+}
+
+
+// 기존 컨테이너 벨트 = 큐
+// 보관용 컨테이너 벨트 = 스택
+
+//// 시뮬레이션
+//int solution(vector<int> order) {
+//    int answer = 0;
+//    queue<int> base;
+//    stack<int> other;
+//
+//    for (int i = 0; i < order.size(); ++i) {
+//        base.push(i + 1);
+//    }
+//
+//    int current_idx = 0;
+//
+//    while (current_idx < order.size()) {
+//        int current_order = order[current_idx];
+//
+//        // 기본 레일 맨 앞이 원하는 상자라면 적재
+//        if (!base.empty() && base.front() == current_order) {
+//            base.pop();
+//            ++current_idx;
+//            ++answer;
+//        }
+//        // 보조 레일 맨 위가 원하는 상자라면 적재
+//        else if (!other.empty() && other.top() == current_order) {
+//            other.pop();
+//            ++current_idx;
+//            ++answer;
+//        }
+//        // 둘 다 아니라면 기본 레일에서 보조 레일로 이동
+//        else {
+//            // 둘다 원하는 상자가 아니면서 base가 비었다면 빠져나와야 함
+//            if (base.empty()) {
+//                break;
+//            }
+//
+//            other.push(base.front());
+//            base.pop();
+//        }
+//    }
+//
+//    return answer;
+//}
+
+int main()
+{
+    solution({ 4,3,1,2,5 });
+    solution({ 5,4,3,2,1 });
+}
