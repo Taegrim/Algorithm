f0791b4bd775a3720e12c44a75f59b73791e2ad4
LeeDongHyun <aopo3948@gmail.com>
2026-05-06 09:35:24 +0900

    연속된 부분 수열의 합, 두 큐 합 같게 만들기
    연속된 부분 수열의 합
     - 벡터의 숫자들을 큐에 넣어가면서 후보자가 될 수 있는 연속된 수열 찾기
     - 후보자 중에서 길이가 짧고 인덱스가 앞인 수열 순서대로 정렬
     - 맨 앞 원소를 반환
    
    두 큐 합 같게 만들기
     - 큐1, 큐2 의 합을 비교해서 큰 큐에서 작은 큐로 넘기기를 반복
     - 무한루프에 빠질 수 있으므로 최대 횟수 조건 필요함
     - 홀수일 경우엔 애초에 같게할 수 없으므로 이전에 -1 리턴

---
+++ Algorithm/Programmers/두 큐 합 같게 만들기.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> queue1, vector<int> queue2) {
+    long long sum1{}, sum2{};
+    queue<int> q1, q2;
+
+    // 두 큐의 총 합 구하고 나누기
+    for (int i : queue1) {
+        sum1 += i;
+        q1.push(i);
+    }
+    for (int i : queue2) {
+        sum2 += i;
+        q2.push(i);
+    }
+
+    // 총 합이 홀수면 불가능하므로 -1
+    if ((sum1 + sum2) % 2 == 1) {
+        return -1;
+    }
+
+    // 무한으로 돌지 않게 loop 최대횟수 걸어두기
+    int answer = 0;
+    int limit = queue1.size() * 3;
+
+    // 큰쪽에서 작은쪽으로 넘기기
+    // 넘기려할 때 비어있다면 -1
+    while (answer <= limit) {
+        if (sum1 == sum2) {
+            return answer;
+        }
+
+        if (sum1 > sum2) {
+            if (q1.empty()) {
+                return -1;
+            }
+
+            int num = q1.front();
+            q2.push(num);
+            q1.pop();
+
+            sum1 -= num;
+            sum2 += num;
+        }
+        else {
+            if (q2.empty()) {
+                return -1;
+            }
+
+            int num = q2.front();
+            q1.push(num);;
+            q2.pop();
+
+            sum2 -= num;
+            sum1 += num;
+        }
+        ++answer;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    solution({ 3,2,7,2 }, { 4,6,5,1 });
+    solution({ 1,2,1,2, }, { 1,10,1,2 });
+    solution({ 1, 1 }, { 1, 5 });
+}
---
+++ Algorithm/Programmers/연속된 부분 수열의 합.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+// 연속된 수열의 인덱스 반환
+// 합은 k, 짧은 수열, 시작 인덱스 작은수열
+
+vector<int> solution(vector<int> sequence, int k) {
+    // 인덱스를 담는 큐
+    queue<int> q;
+    int sum = 0;
+
+    // 정답이 될 수 있는 후보자 배열
+    vector<pair<int, int>> candidate;
+
+    for (int i = 0; i < sequence.size(); ++i) {
+
+        // sum + sequence[i] 비교
+        // k를 넘어서면 맨앞에서 계속 제거
+        while (!q.empty() &&sum + sequence[i] > k) {
+            sum -= sequence[q.front()];
+            q.pop();
+        }
+
+        q.push(i);
+        sum += sequence[i];
+
+        // 연속된 수열 만족한다면 후보자 등록
+        if (sum == k) {
+            candidate.push_back({ q.front(), q.back() });
+        }
+    }
+
+    // 후보자에서 짧고 인덱스가 앞인 수열 선택
+    sort(candidate.begin(), candidate.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+        int len_a = a.second - a.first;
+        int len_b = b.second - b.first;
+
+        // 길이가 같다면 인덱스 낮은게 앞으로 오게
+        if (len_a == len_b) {
+            return a.first < b.first;
+        }
+
+        // 길이가 다르면 길이 짧은게 앞으로 오게
+        return len_a < len_b;
+        });
+
+    return { candidate.front().first, candidate.front().second };
+}
+
+int main()
+{
+    solution({ 1,2,3,4,5 }, 7);
+    solution({ 1, 1, 1, 2, 3, 4, 5 }, 5);
+    solution({ 2, 2, 2, 2, 2 }, 6);
+}
