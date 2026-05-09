e5416982b05ecb75d3c58ba346e17fa320dc42d7
LeeDongHyun <aopo3948@gmail.com>
2026-05-09 20:55:27 +0900

    행렬 테두리 회전하기, 전력망을 둘로 나누기
    행렬 테두리 회전하기
     - 직접 회전시키면서 최솟값을 구하는 방식으로 풀이함
     - 이전에 삼각 달팽이처럼 dir를 미리 작성하고 조건에따라 방향을 바꾸면서 체크
    
    전력망을 둘로 나누기
     - 간선을 하나씩 끊어가면서 BFS로 송전탑 개수 차이 구함

---
+++ Algorithm/Programmers/전력망을 둘로 나누기.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+
+#include <vector>
+#include <queue>
+#include <limits>
+#include <algorithm>
+#include <cstdlib>
+
+using namespace std;
+
+// wires[0] <-> wires[1] 간선
+
+// 간선 bfs 로 탐색하면서 연결된 개수 세기
+int CountTowers(const vector<vector<int>>& towers, int start, int blocked, int n)
+{
+    vector<bool> visited(n + 1, false);
+
+    int count = 0;
+    queue<int> next_towers;
+
+    visited[start] = true;
+    next_towers.push(start);
+
+    //탐색 가능하면 큐에 넣어가면서 탐색
+    while (!next_towers.empty()) {
+        int current = next_towers.front();
+        next_towers.pop();
+        ++count;
+
+        for (int next : towers[current]) {
+            if (!visited[next] && next != blocked) {
+                visited[next] = true;
+                next_towers.push(next);
+            }
+        }
+    }
+
+    return count;
+}
+
+int solution(int n, vector<vector<int>> wires) {
+    vector<vector<int>> towers(n + 1);
+
+    // 양방향 간선 연결
+    for (const auto& wire : wires) {
+        towers[wire[0]].push_back(wire[1]);
+        towers[wire[1]].push_back(wire[0]);
+    }
+
+    int min_diff = numeric_limits<int>::max();
+
+    // 하나씩 연결 끊어가면서 확인
+    for (const auto& wire : wires) {
+        int tower1 = wire[0];
+        int tower2 = wire[1];
+
+        int count1 = CountTowers(towers, tower1, tower2, n);
+
+        // 한쪽을 구하면 나머진 n - count 가 됨
+        int count2 = n - count1;
+
+        min_diff = min(min_diff, abs(count1 - count2));
+    }
+
+    return min_diff;
+}
+
+int main()
+{
+    solution(9, { {1,3}, {2,3}, {3,4}, {4,5}, {4,6}, {4,7}, {7,8}, {7,9} });
+    solution(4, { {1,2}, {2,3}, {3,4} });
+    solution(7, { {1,2}, {2,7}, {3,7}, {3,4}, {4,5}, {6,7} });
+}
---
+++ Algorithm/Programmers/행렬 테두리 회전하기.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+#include <limits>
+
+using namespace std;
+
+// 2차원 배열과 시작점, 끝점을 받아서 회전시키는 함수
+int Rotation(vector<vector<int>>& maps, int start_row, int start_col, int end_row, int end_col)
+{
+    // 방향, 오른쪽, 아래, 왼쪽, 위
+    int dir = 0;
+    int dr[4] = { 0, 1, 0, -1 };
+    int dc[4] = { 1, 0, -1, 0 };
+
+    // 회전해야할 횟수, 테두리 길이
+    int length = 2 * (end_row - start_row + 1) + 2 * (end_col - start_col + 1) - 4;
+
+    int row = start_row;
+    int col = start_col;
+
+    int prev = maps[row][col];
+    int min_val = numeric_limits<int>::max();
+
+    // 앞의 내용을 이전 내용으로 덮어씀
+    for (int i = 0; i < length; ++i) {
+        int nr = row + dr[dir];
+        int nc = col + dc[dir];
+
+        // 현재 상태 저장, prev 갱신용
+        int cur = maps[nr][nc];
+
+        // 이전내용으로 덮어쓰고, prev 갱신, 최솟값 갱신
+        maps[nr][nc] = prev;
+        prev = cur;
+        min_val = min(min_val, cur);
+
+        row = nr;
+        col = nc;
+
+        // 모서리에 도달하면 방향 변경
+        if ((nr == start_row && (nc == start_col || nc == end_col)) ||
+            (nr == end_row && (nc == start_col || nc == end_col)))
+        {
+            ++dir;
+        }
+    }
+
+    return min_val;
+}
+
+vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
+    vector<int> answer;
+
+    vector<vector<int>> maps(rows, vector<int>(columns));
+
+    int num = 0;
+    for (vector<int>& v : maps) {
+        for (int& i : v) {
+            i = ++num;
+        }
+    }
+
+    for (const vector<int>& v : queries) {
+        answer.push_back(Rotation(maps, v[0] - 1, v[1] - 1, v[2] - 1, v[3] - 1));
+    }
+
+
+    return answer;
+}
+
+int main()
+{
+    solution(6, 6, { { 2, 2, 5, 4 }, { 3, 3, 6, 6 }, { 5, 1, 6, 3 } });
+    solution(3, 3, { { 1,1,2,2},{1,2,2,3},{2,1,3,2},{2,2,3,3 } });
+    solution(100, 97, { {1,1,100,97} });
+}
