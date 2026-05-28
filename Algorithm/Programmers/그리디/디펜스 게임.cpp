537074b3fcb0227af1f01d908e327b9ae88c45b4
LeeDongHyun <aopo3948@gmail.com>
2026-05-28 10:16:29 +0900

    리코쳇 로봇, 디펜스 게임
    리코쳇 로봇:
    - 그래프 탐색 문제
    - 정석은 BFS지만 DFS를 먼저 떠올려서 풀이함
    - 최소거리, 최단거리, 간선비용1 인 경우 BFS를 떠올려야함
    
    디펜스 게임:
    - 우선순위 큐 + 그리디 문제
    - 이전까지 지나온 웨이브 중에서 적이 많았던 웨이브에 무적권을 사용했다 가정해야 함

---
+++ Algorithm/Programmers/그리디/디펜스 게임.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+// 병사n, 매 라운드 enemy[i] 마리
+// k 번의 무적권으로 enemy[i] 만큼의 소모 막을수 있음
+
+int solution(int n, int k, vector<int> enemy) {
+
+    // 여태까지 진행한 웨이브들을 적이 많은 순서대로 저장하는 큐
+    priority_queue<int> pq;
+
+    int current_wave = 0;
+    for (; current_wave < enemy.size(); ++current_wave) {
+        // 일단 병사 수 감소, 우선순위 큐에 저장
+        n -= enemy[current_wave];
+        pq.push(enemy[current_wave]);
+
+        // 라운드 종료 위기인데 무적권이 남아있다면
+        // 무적권을 사용해서 가장 많았던 웨이브를 회복함
+        if (n < 0) {
+            if (k <= 0) {
+                break;
+            }
+
+            n += pq.top();
+            pq.pop();
+            --k;
+        }
+    }
+
+    return current_wave;
+}
+
+int main()
+{
+    solution(7, 3, { 4,2,4,5,3,3,1 });
+    solution(2, 4, { 3,3,3,3 });
+}
---
+++ Algorithm/Programmers/탐색/리코쳇 로봇.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// r에서 시작, 목표 : G, 장애물 : D
+
+constexpr int MAX = 1e9;
+
+bool CheckBoard(const vector<string>& board, int row, int col)
+{
+    if (row >= 0 && row < board.size() && col >= 0 && col < board[0].size()) {
+        if (board[row][col] != 'D') {
+            return true;
+        }
+    }
+    return false;
+}
+
+void DFS(const vector<string>& board, vector<vector<int>>& visited,
+    int row, int col, int goal_row, int goal_col, int depth)
+{
+    visited[row][col] = depth;
+
+    if (row == goal_row && col == goal_col) {
+        return;
+    }
+
+    int dr[4] = { -1, 1, 0, 0 };
+    int dc[4] = { 0, 0, -1, 1 };
+
+    for (int i = 0; i < 4; ++i) {
+        int nr = row;
+        int nc = col;
+
+        // 한 방향으로 직진하다가 충돌하면 종료
+        while (CheckBoard(board, nr + dr[i], nc + dc[i])) {
+            nr += dr[i];
+            nc += dc[i];
+        }
+
+        // 가려는 곳의 시도횟수가 현재 시도횟수보다 클때만 진입
+        if (depth + 1 < visited[nr][nc]) {
+            DFS(board, visited, nr, nc, goal_row, goal_col, depth + 1);
+        }
+    }
+}
+
+int solution(vector<string> board) {
+    int answer = 0;
+
+    vector<vector<int>> visited(board.size(), vector<int>(board[0].size(), MAX));
+
+    int start_row{}, start_col{};
+    int goal_row{}, goal_col{};
+
+    for (int i = 0; i < board.size(); ++i) {
+        for (int j = 0; j < board[i].size(); ++j) {
+            if (board[i][j] == 'R') {
+                start_row = i;
+                start_col = j;
+            }
+            else if (board[i][j] == 'G') {
+                goal_row = i;
+                goal_col = j;
+            }
+        }
+    }
+
+    DFS(board, visited, start_row, start_col, goal_row, goal_col, 0);
+
+    answer = visited[goal_row][goal_col];
+    if (answer == MAX) {
+        answer = -1;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution({ "...D..R", ".D.G...", "....D.D", "D....D.", "..D...." });
+    solution({ ".D.R", "....", ".G..", "...D" });
+}
