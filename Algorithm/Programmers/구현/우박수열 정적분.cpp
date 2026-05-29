43a372710c70e7cba6799e7d3ec0f52d8bcd965f
LeeDongHyun <aopo3948@gmail.com>
2026-05-29 10:27:39 +0900

    우박수열 정적분, 두 원 사이의 정수 쌍
    우박수열 정적분
    - 콜라츠 수열로 1을 만들면서 그때그때 적분 결과를 저장하고 이를 이용하는 문제
    - 누적합 사용을 놓침, 사다리꼴 공식 사용을 놓침
    
    두 원 사이의 정수 쌍
    - 이전 문제인 점찍기와 비슷하게 풀이하는 문제
    - 안쪽원의 경우 경계를 포함해야하므로 올림처리 해야함

---
+++ Algorithm/Programmers/구현/우박수열 정적분.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// a부터 n-b 까지 정적분
+
+// 적분은 윗 삼각형 + 아래 사각형, x 길이는 1
+// 적분 = abs(a-b)/2 + min(a,b)
+//  사다리꼴 -> (a + b) / 2 였음
+
+vector<double> solution(int k, vector<vector<int>> ranges) {
+    vector<double> answer;
+
+    vector<double> rects;
+    double rect{};
+    int left{ k }, right{ k };
+    int count{};
+
+    // 콜라츠 수열을 따라가면서 정적분 결과 저장
+    while (k != 1) {
+        // 짝수일때
+        if (k % 2 == 0) {
+            k /= 2;
+        }
+        else {
+            k = k * 3 + 1;
+        }
+        right = k;
+
+        rect = (double)abs(left - right) / 2.0 + (double)min(left, right);
+        //rect = (double)(left + right) / 2.0;
+        rects.push_back(rect);
+
+        left = right;
+        ++count;
+    }
+
+    for (const vector<int>& range : ranges) {
+        int left = range[0];
+        int right = count + range[1];
+        rect = 0.0;
+
+        if (left > right) {
+            answer.push_back(-1.0);
+            continue;
+        }
+
+        for (int i = left; i < right; ++i) {
+            rect += rects[i];
+        }
+        answer.push_back(rect);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(5, { {0,0}, {0,-1}, {2,-3},{3,-3} });
+    solution(3, { {0,0}, {1,-2}, {3,-3} });
+}
---
+++ Algorithm/Programmers/수학/두 원 사이의 정수 쌍.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+// 중심원, 반지름 r1, r2
+// 두 원 사이(표면 포함)에 존재하는 점의 개수
+// 점찍기와 유사한 문제
+
+
+long long solution(int r1, int r2) {
+    long long answer = 0;
+
+    long long r1_sqaure = (long long)r1 * r1;
+    long long r2_sqaure = (long long)r2 * r2;
+
+    // 1사분면 계산
+    for (long long i = 1; i <= r2; ++i) {
+        // 바깥원은 넘으면 안되므로 정수변환 자동 내림처리
+        long long max_y = (long long)sqrt(r2_sqaure - i * i);
+        long long min_y = 0;
+
+        // r1보다 i가 작을때만 처리
+        // 안쪽원의 경계는 포함해야하므로 올림처리
+        if (i < r1) {
+            min_y = (long long)ceil(sqrt(r1_sqaure - i * i));
+        }
+
+        answer += max_y - min_y + 1;
+    }
+
+    // 4사분면까지 계산
+    answer *= 4;
+
+    return answer;
+}
+
+int main()
+{
+    solution(2, 3);
+}
