55c216ab5d19f8b40646d69bf8dabae528628b22
LeeDongHyun <aopo3948@gmail.com>
2026-06-01 10:40:30 +0900

    혼자서하는 틱택토, 이모티콘 할인행사
    혼자서 하는 틱택토
    - 턴 실수를 했는지 확인하고, 누가 이겼는지를 판단해서 확인하는 방식으로 풀이함
    
    이모티콘 할인행사
    - 선택지가 적어 완전탐색으로 풀어야 하는 문제
    - 완전탐색으로 가능한 할인율 조합을 구하고 가입자 수와 판매액을 체크해서 풀이함

---
+++ Algorithm/Programmers/구현/혼자서 하는 틱택토.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 차례 잘못되는 실수, 승리해도 계속하는 실수
+// - O가 X보다 2개 많거나, X가 O보다 많으면 차례 실수함
+
+
+enum { circle, cross, COUNT };
+constexpr int SIZE = 3;
+
+bool CheckWin(const vector<string>& board, char mark)
+{
+    // 가로 완성
+    for (const string& s : board) {
+        if (count(s.begin(), s.end(), mark) == SIZE) {
+            return true;
+        }
+    }
+
+    // 세로 완성
+    for (int col = 0; col < SIZE; ++col) {
+        if (board[0][col] == mark && board[1][col] == mark && board[2][col] == mark) {
+            return true;
+        }
+    }
+
+    // 대각선
+    if (board[0][0] == mark && board[1][1] == mark && board[2][2] == mark) {
+        return true;
+    }
+    // 대각선
+    if (board[2][0] == mark && board[1][1] == mark && board[0][2] == mark) {
+        return true;
+    }
+
+    return false;
+}
+
+int solution(vector<string> board) {
+    int counts[COUNT]{ 0, };
+
+    // 놓여있는 표시 확인
+    for (const string& s : board) {
+        for (char c : s) {
+            if (c == 'O') {
+                ++counts[circle];
+            }
+            else if(c == 'X') {
+                ++counts[cross];
+            }
+        }
+    }
+
+    bool wins[COUNT]{ false, };
+
+    wins[circle] = CheckWin(board, 'O');
+    wins[cross] = CheckWin(board, 'X');
+
+    // 둘다 이겼다면 잘못둔것
+    if (wins[circle] && wins[cross]) {
+        return 0;
+    }
+
+    // O를 마지막으로 둠
+    if (counts[circle] == counts[cross] + 1) {
+        if (wins[cross]) {
+            return 0;
+        }
+    }
+    // X를 마지막으로 둠
+    else if (counts[circle] == counts[cross]) {
+        if (wins[circle]) {
+            return 0;
+        }
+    }
+    // 차례를 실수함
+    else {
+        return 0;
+    }
+
+    return 1;
+}
+
+int main()
+{
+    solution({ "O.X", ".O.", "..X" });
+    solution({ "OOO", "...", "XXX" });
+    solution({ "...", ".X.", "..." });
+    solution({ "...", "...", "..." });
+}
---
+++ Algorithm/Programmers/재귀/이모티콘 할인행사.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 완전탐색 DFS 기본 상태
+/*
+void DFS(int index)
+{
+    if (index == 선택할_개수) {
+        현재까지 만든 조합 평가;
+        return;
+    }
+
+    for (가능한 선택지) {
+        현재 index에 선택 저장;
+        DFS(index + 1);
+    }
+}
+*/
+
+// 1. 플러스 가입자 최대, 2. 판매액 최대
+// N명에게 M개를 할인해서 판매
+// 일정 비율 이상 할인은 모두 구매, 구매비용 일정 넘어가면 취소하고 플러스 가입
+
+int max_users = -1;
+int max_sales = -1;
+vector<int> discounts;
+
+void DFS(const vector<vector<int>>& users, vector<int>& emoticons, int index)
+{
+    if (index == emoticons.size()) {
+        int user_count = 0;
+        int total_sales = 0;
+
+        for (const vector<int>& v : users) {
+            int rate = v[0];    // 구매할 할인율
+            int limit = v[1];   // 플러스 가입 최소 비용
+            int prices = 0;     // 총 구매 금액
+
+            // 이모티콘별 할인율 체크
+            for (int i = 0; i < discounts.size(); ++i) {
+                if (discounts[i] >= rate) {
+                    int price = emoticons[i] * (100 - discounts[i]) / 100;
+                    prices += price;
+                }
+            }
+
+            // 구매한도를 넘어서면 모두 취소하고 플러스 가입
+            if (prices >= limit) {
+                ++user_count;
+                prices = 0;
+            }
+            total_sales += prices;
+        }
+
+        // 기존 가입자수보다 많다면 갱신
+        if (user_count > max_users) {
+            max_users = user_count;
+            max_sales = total_sales;
+        }
+        // 가입자수가 똑같다면 판매비용만 갱신
+        else if (user_count == max_users) {
+            max_sales = max(max_sales, total_sales);
+        }
+
+        return;
+    }
+
+    // 할인율 완전탐색
+    for (int discount : {10, 20, 30, 40}) {
+        discounts[index] = discount;
+        DFS(users, emoticons, index + 1);
+    }
+}
+
+vector<int> solution(vector<vector<int>> users, vector<int> emoticons) {
+    discounts.resize(emoticons.size());
+
+    DFS(users, emoticons, 0);
+
+    return { max_users, max_sales };
+}
+
+int main()
+{
+    solution({ {40, 10000}, {25,10000} }, { 7000, 9000 });
+    solution({ {40, 2900},{23, 10000},{11, 5200},{5, 5900},{40, 3100},{27, 9200},{32, 6900} },
+        { 1300, 1500, 1600, 4900 });
+}
