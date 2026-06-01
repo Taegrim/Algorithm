ae5c8f2ba519de383fb34310d331f6b48d5c189c
LeeDongHyun <aopo3948@gmail.com>
2026-06-01 23:03:07 +0900

    요격 시스템, 양궁 대회
    요격 시스템
    - 미사일을 정렬하고 앞에서부터 구간을 체크하거나 끝점을 그리디로 체크하는 방식
    
    양궁 대회
    - 이모티콘 할인문제와 비슷하게 백트래킹 + 완전탐색으로 풀어야 하는 문제
    - 남은 화살 소진, 점수차가 같다면 낮은 점수가 우선인 경우를 추가로 체크해야 함

---
+++ Algorithm/Programmers/그리디/요격 시스템.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 미사일 최소로 모든 폭격 미사일 요격
+
+// A의 폭격 미사일 - x축에 평행한 직선, 정수쌍 (s,e)로 표현, s랑 e는 포함 X
+// B는 x좌표에서 y축에 수평이 되게 발사해서 걸친 모든 미사일 관통해 요격
+//  - 요격 미사일은 실수 x에서 발사 가능
+
+
+// 구간을 나누는 방식
+int solution(vector<vector<int>> targets) {
+    int answer = 0;
+
+    // x가 작은 순으로 정렬
+    sort(targets.begin(), targets.end());
+
+    int left = targets[0][0];
+    int right = targets[0][1];
+
+    for (int i = 1; i < targets.size(); ++i) {
+        int new_left = targets[i][0];
+        int new_right = targets[i][1];
+
+        // 새 미사일이 구간 내에 들어오면 구간 갱신
+        if (new_left < right) {
+            left = max(left, new_left);
+            right = min(right, new_right);
+        }
+        // 구간 밖이라면 기존은 요격하고 새로운 구간 갱신
+        else {
+            ++answer;
+            left = new_left;
+            right = new_right;
+        }
+    }
+    // 마지막은 항상 요격
+    ++answer;
+
+    return answer;
+}
+
+
+// 끝점 기준 체크
+int solution(vector<vector<int>> targets) {
+    int answer = 1;
+
+    // 끝점 기준 정렬
+    sort(targets.begin(), targets.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a[1] < b[1];
+        });
+
+    int end = targets[0][1];
+
+    for (int i = 1; i < targets.size(); ++i) {
+        int start = targets[i][0];
+
+        // 새 미사일의 시작점이 끝점보다 크거나 같다면 요격 불가능
+        // 한번 요격하고 갱신
+        if (start >= end) {
+            ++answer;
+            end = targets[i][1];
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution({ {4,5} ,{4,8},{10,14},{11,13},{5,12},{3,7},{1,4} });
+}
---
+++ Algorithm/Programmers/재귀/양궁대회.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 가장 큰 점수차로 이겨야함
+// 같은 점수는 1개 많게 쏴야함
+// 지거나 비기면 -1 리턴
+
+// 가장 큰 점수차로 우승할 수 있는 경우가 많다면 가장 낮은 점수를 많이 맞도록
+
+vector<int> shoots;
+vector<int> answer;
+int max_diff = 0;
+
+bool Lower(const vector<int>& current, const vector<int>& prev)
+{
+    // 뒤에서부터 화살 확인
+    for (int i = 10; i >= 0; --i) {
+        // 지금이 이전보다 낮은 점수에 화살이 많다면 true
+        if (current[i] > prev[i]) {
+            return true;
+        }
+        // 아니면 false
+        else if (current[i] < prev[i]) {
+            return false;
+        }
+    }
+
+    return false;
+}
+
+void JudgeScore(const vector<int>& info)
+{
+    int ryan_score = 0;
+    int apeach_score = 0;
+
+    for (int i = 0; i < info.size(); ++i) {
+        int score = 10 - i;
+
+        // 라이언이 더 많이 쐈다면 라이언 점수
+        if (shoots[i] > info[i]) {
+            ryan_score += score;
+        }
+        // 라이언이 적게쐈거나 같은데 어피치가 쐈다면 어피치 점수
+        else if (info[i] > 0) {
+            apeach_score += score;
+        }
+    }
+
+    int current_diff = ryan_score - apeach_score;
+
+    // 지거나 비기는 경우
+    if (current_diff <= 0) {
+        return;
+    }
+
+    // 점수차 갱신할 수 있으면 갱신
+    if (current_diff > max_diff) {
+        max_diff = current_diff;
+        answer = shoots;
+    }
+    // 점수차가 똑같다면 낮은 점수에 많이 쏜거로 변경
+    else if (current_diff == max_diff && Lower(shoots, answer)) {
+        answer = shoots;
+    }
+}
+
+void DFS(const vector<int>& info, int index, int remain_arrow)
+{
+    if (index == info.size()) {
+        // 남은 화살은 전부 낮은곳에 몰아주기
+        shoots[10] += remain_arrow;
+
+        JudgeScore(info);
+
+        shoots[10] -= remain_arrow;
+
+        return;
+    }
+
+    int count = info[index] + 1;
+
+    // 현재 점수에 한발 더 쏘고 넘어감
+    if (remain_arrow >= count) {
+        shoots[index] = count;
+        DFS(info, index + 1, remain_arrow - count);
+        shoots[index] = 0;
+    }
+
+    // 안쏘고 넘어감
+    DFS(info, index + 1, remain_arrow);
+}
+
+vector<int> solution(int n, vector<int> info) {
+    shoots.assign(info.size(), 0);
+    answer.clear();
+
+    max_diff = 0;
+
+    DFS(info, 0, n);
+
+    if (answer.empty()) {
+        return { -1 };
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(5, { 2,1,1,1,0,0,0,0,0,0,0 });
+    solution(1, { 1,0,0,0,0,0,0,0,0,0,0 });
+    solution(9, { 0,0,1,2,0,1,1,1,1,1,1 });
+    solution(10, { 0,0,0,0,0,0,0,0,3,4,3 });
+}
