d6b3f7920e43ca003874b77f04887fd709a1260d
LeeDongHyun <aopo3948@gmail.com>
2026-06-03 16:06:45 +0900

    유사 칸토어 비트열, 당구 연습
    유사 칸토어 비트열
    - 재귀를 이용해서 n이 1일떄까지 내려간다음 직전 숫자를 구하는 방식으로 풀이함
    - 메모리 제한이 있기 때문에 직접 문자열을 만드는 방식은 불가능함
    
    당구 연습
    - 4가지 벽면에 대해 길이를 구하는 방식으로 풀이함
    - 반사 벡터를 뒤집어서 직선벡터로 만들어서 보면 길이를 구하기 쉬움

---
+++ Algorithm/Programmers/수학/당구 연습.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+
+#include <vector>
+#include <cmath>
+#include <limits>
+
+using namespace std;
+
+// 리스트 -> 공들의 위치,  공의 시작점은 같음
+// 가로 m 세로 n, 시작공좌표 startX, startY, 목표 공 좌표 balls
+
+// 벽에 튕긴다 -> 반사벡터를 뒤집으면 공에서부터 직선벡터가 나옴
+
+int GetSquaredLength(int m, int n, int start_x, int start_y, int ball_x, int ball_y)
+{
+    int length = numeric_limits<int>::max();
+    int x{}, y{};
+
+    // 왼쪽 벽 반사, y가 같고, 치려는 공이 왼쪽이라면 불가능
+    if (!(start_y == ball_y && ball_x < start_x)) {
+        x = start_x + ball_x;
+        y = abs(ball_y - start_y);
+
+        length = min(length, x * x + y * y);
+    }
+
+    // 오른쪽 벽 반사, y가 같고, 치려는 공이 오른쪽이라면 불가능
+    if (!(start_y == ball_y && ball_x > start_x)) {
+        x = m - start_x + m - ball_x;
+        y = abs(ball_y - start_y);
+
+        length = min(length, x * x + y * y);
+    }
+
+    // 위쪽 벽 반사, x가 같고, 치려는 공이 위쪽이라면 불가능
+    if (!(start_x == ball_x && ball_y > start_y)) {
+        x = abs(ball_x - start_x);
+        y = n - start_y + n - ball_y;
+
+        length = min(length, x * x + y * y);
+    }
+
+    // 아래쪽 벽 반사, x가 같고, 치려는 공이 아래쪽이라면 불가능
+    if (!(start_x == ball_x && ball_y < start_y)) {
+        x = abs(ball_x - start_x);
+        y = start_y + ball_y;
+
+        length = min(length, x * x + y * y);
+    }
+
+    return length;
+}
+
+vector<int> solution(int m, int n, int startX, int startY, vector<vector<int>> balls) {
+    vector<int> answer;
+    answer.reserve(balls.size());
+
+    for (const auto& ball : balls) {
+        answer.push_back(GetSquaredLength(m, n, startX, startY, ball[0], ball[1]));
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(10, 10, 3, 7, { {7,7}, {2,7}, {7,3}, {4, 6} });
+}
---
+++ Algorithm/Programmers/재귀/유사 칸토어 비트열.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 직전의 비트열에서 1을 11011로 치환하고 0을 00000으로 치환함
+// 0번째 비트열은 "1"
+
+//               1
+//   1     1     0     1     1
+// 11011 11011 00000 11011 11011
+
+// 5의 단위로 늘어남
+
+long long PrevNumber(long long num, int n)
+{
+    // 1번째 칸토어 비트열 확인
+    if (n == 1) {
+        if (num == 2) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
+
+    // 현재 속해있는 구간에서 몇번째 숫자인지
+    long long current = num % 5;
+
+    // n 이 1까지 내려가면서 이전 숫자들을 구하는 방식
+    long long prev = PrevNumber(num / 5, n - 1);
+
+    // 이전 숫자가 1이면 현재 가운데 숫자는 0, 나머진 1
+    if (prev == 1) {
+        if (current == 2) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
+    // 이전 숫자가 0이면 무조건 0 변환
+    else {
+        return 0;
+    }
+}
+
+int solution(int n, long long l, long long r) {
+    int answer = 0;
+
+    // 직전 숫자를 알 수 있다면?
+    for (long long ll = l; ll <= r; ++ll) {
+        long long prev = PrevNumber(ll - 1, n);
+
+        if (prev == 1) {
+            ++answer;
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(2, 4, 17);
+}
