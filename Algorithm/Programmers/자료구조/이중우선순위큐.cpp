8ad0b14dd7f2c427d3f8c5e694abc8cee05ed949
LeeDongHyun <aopo3948@gmail.com>
2026-06-04 21:03:36 +0900

    이중 우선순위 큐
    이중우선순위큐
    - multiset, vector를 이용하면 풀 수 있는 문제
    - stringstream 을 이용해서 문자열을 파싱하고 이를 이용해 요구에 맞게 구현하면 되는 문제

---
+++ Algorithm/Programmers/자료구조/이중우선순위큐.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+#include <set>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<string> operations) {
+    vector<int> answer;
+
+    multiset<int> ms;
+
+    for (const auto& operation : operations) {
+        stringstream ss(operation);
+        string mode{}, num{};
+
+        ss >> mode >> num;
+
+        if (mode == "I") {
+            int n = stoi(num);
+            ms.insert(n);
+        }
+        else {
+            if (ms.empty()) {
+                continue;
+            }
+
+            if (num == "-1") {
+                ms.erase(ms.begin());
+            }
+            else {
+                auto it = prev(ms.end());
+                ms.erase(it);
+            }
+        }
+    }
+
+    if (ms.empty()) {
+        return { 0, 0 };
+    }
+
+    return { *prev(ms.end()), *ms.begin() };
+}
+
+int main()
+{
+    solution({ "I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1" });
+    solution({ "I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333" });
+}
