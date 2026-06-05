d67e61dd3a6780bc53ffea72b203260742771d1d
LeeDongHyun <aopo3948@gmail.com>
2026-06-05 09:51:18 +0900

    네트워크, 단어 변환
    네트워크
    - 그래프를 BFS를 이용해서 연결된 집합이 몇개인지 확인하는 문제
    
    단어 변환
    - 알파벳 개수차가 1인 단어들로 바꿔가며 최소 횟수를 구하는 문제
    - 처음에 DFS로 풀었지만 BFS가 더 적합한 방식임

---
+++ Algorithm/Programmers/탐색/네트워크.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<bool> visited;
+
+bool BFS(const vector<vector<int>>& computers, int start)
+{
+    if (visited[start]) {
+        return false;
+    }
+    queue<int> q;
+
+    q.push(start);
+    visited[start] = true;
+
+    while (!q.empty()) {
+        int current = q.front();
+        q.pop();
+
+        const auto& computer = computers[current];
+        for (int i = 0; i < computer.size(); ++i) {
+            if (i == current) continue;
+
+            if (computer[i] == 1 && !visited[i]) {
+                q.push(i);
+                visited[i] = true;
+            }
+        }
+    }
+
+    return true;
+}
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+
+    visited.assign(computers.size(), false);
+
+    for (int i = 0; i < computers.size(); ++i) {
+        if (BFS(computers, i)) {
+            ++answer;
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(3, { {1,1,0}, {1,1,0}, {0,0,1} });
+    solution(3, { {1,1,0}, {1,1,1}, {0,1,1} });
+}
---
+++ Algorithm/Programmers/탐색/단어 변환.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+// begin -> target 으로 단어 변경
+// 한번에 알파벳 하나만 가능, words에 있는 단어로만 변환 가능
+
+vector<bool> visited;
+int answer = 1e9;
+
+int CountDiff(const string& a, const string& b)
+{
+    int count = 0;
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            ++count;
+        }
+    }
+    return count;
+}
+
+void DFS(const vector<string>& words, const string& begin, const string& target, int depth)
+{
+    if (begin == target) {
+        answer = min(answer, depth);
+        return;
+    }
+
+    for (int i = 0; i < words.size(); ++i) {
+        // 방문하지 않았고, 알파벳 차이가 1개라면 변환 시도
+        if (!visited[i] && CountDiff(begin, words[i]) == 1) {
+            visited[i] = true;
+            DFS(words, words[i], target, depth + 1);
+            visited[i] = false;
+        }
+    }
+}
+
+void BFS(const vector<string>& words, const string& begin, const string& target)
+{
+    queue<pair<string,int>> q;
+    q.push({ begin, 0 });
+
+    while (!q.empty()) {
+        auto [str, depth] = q.front();
+        q.pop();
+
+        if (str == target) {
+            answer = min(answer, depth);
+            break;
+        }
+
+        for (int i = 0; i < words.size(); ++i) {
+            if (!visited[i] && CountDiff(str, words[i]) == 1) {
+                visited[i] = true;
+                q.push({ words[i],  depth + 1});
+            }
+        }
+    }
+}
+
+int solution(string begin, string target, vector<string> words) {
+    visited.assign(words.size(), false);
+
+    // words에 target이 있어야 함
+    if (find(words.begin(), words.end(), target) == words.end()) {
+        return 0;
+    }
+
+    //DFS(words, begin, target, 0);
+    BFS(words, begin, target);
+
+    if (answer == 1e9) {
+        return 0;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution("hit", "cog", { "hot", "dot", "dog", "lot", "log", "cog" });
+    solution("hit", "cog", { "hot", "dot", "dog", "lot", "log" });
+}
