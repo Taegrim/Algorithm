577abb80f6c48374b6a12350e9e8d6635d6e8b36
LeeDongHyun <aopo3948@gmail.com>
2026-06-05 11:33:32 +0900

    양과 늑대
    양과 늑대
    - 현재는 갈 수 없지만 나중에 갈 수 있으므로 갈 수 있는 길을 저장해야 함
    - DFS + 후보 경로 를 이용해서 풀이함

---
+++ Algorithm/Programmers/탐색/양과 늑대.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+// info = i번 노드에 양 or 늑대 정보
+// edges = 간선
+// DFS + 갈 수 있는 길을 저장해야할듯
+
+vector<vector<int>> graph;
+int answer;
+
+void DFS(const vector<int>& info, unordered_set<int> candidates, int current, int sheep, int wolf)
+{
+    if (info[current] == 0) {
+        ++sheep;
+    }
+    else {
+        ++wolf;
+    }
+
+    if (wolf >= sheep) {
+        return;
+    }
+
+    answer = max(answer, sheep);
+
+    // 현재 노드에서 갈 수 있는 길 후보 등록
+    for (int next : graph[current]) {
+        candidates.insert(next);
+    }
+
+    // 후보자 명단에서 경로 선택
+    for (int next : candidates) {
+        // 다음 경로를 복사해서 전달함
+        // 한 경로로 들어갔을때 다른 경로들이 남아있어야 하기 때문
+        unordered_set<int> nexts = candidates;
+        nexts.erase(next);
+
+        DFS(info, nexts, next, sheep, wolf);
+    }
+}
+
+int solution(vector<int> info, vector<vector<int>> edges) {
+    answer = 0;
+    graph.assign(info.size(), vector<int>());
+
+    for (const auto& v : edges) {
+        int parent = v[0];
+        int child = v[1];
+
+        graph[parent].push_back(child);
+    }
+
+    DFS(info, {}, 0, 0, 0);
+
+    return answer;
+}
+
+int main()
+{
+    solution({ 0,0,1,1,1,0,1,0,1,0,1,1 },
+        { {0,1} ,{1,2},{1,4},{0,8},{8,7},{9,10},{9,11},{4,3},{6,5},{4,6},{8,9} });
+    solution({ 0,1,0,1,1,0,1,0,0,1,0 },
+        { {0,1} ,{0,2},{1,3},{1,4},{2,5},{2,6},{3,7},{4,8},{6,9},{9,10} });
+}
