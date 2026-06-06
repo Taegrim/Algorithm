f64b851a1473a1c32e2dd0d213e63995ebec3e5e
LeeDongHyun <aopo3948@gmail.com>
2026-06-06 16:33:36 +0900

    베스트 앨범, 가장 먼 노드
    베스트 앨범
    - 장르별로 최대 재생 횟수와 장르별로 곡들을 따로 관리해야하는 문제
    - 두개의 unordered_map으로 장르별 정보를 저장하고, 이후에 정렬하는 방식으로 풀이함
    
    가장 먼 노드
    - 그래프, 간선 비용 1로 동일, 최단 거리 이므로 BFS로 푸는 문제
    - BFS 중간에 최대 거리와 개수를 갱신하는 방식으로 풀이함

---
+++ Algorithm/Programmers/자료구조/베스트앨범.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+// 장르별로 가장 많이 재생된 노래
+// 1. 속한 노래가 많이 재생된 장르
+// 2. 장르 내에서 많이 재생된 노래
+// 3. 장르 내에서 재생횟수가 같다면 고유번호가 낮은 노래
+
+// 장르별로 2개까지만 모아서 앨범 출시
+
+// 베스트 앨범에 들어갈 고유번호 순서대로
+
+struct Data {
+    int count;
+    int index;
+};
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+
+    unordered_map <string, int> um1;
+
+    // 장르별 데이터
+    unordered_map<string, vector<Data>> um2;
+
+    // 어떤 장르가 많이 재생됐는지
+    // 장르 중에서 많이 재생된 노래가 뭔지
+    for (int i = 0; i < genres.size(); ++i) {
+        const string& genre = genres[i];
+        const int& play = plays[i];
+
+        um1[genre] += play;
+        um2[genre].push_back({ play, i });
+    }
+
+    // 장르별 최대 재생수로 정렬
+    vector<pair<string, int>> v;
+    for (const auto& [genre, count] : um1) {
+        v.push_back({ genre, count });
+    }
+    sort(v.begin(), v.end(), [](const pair<string, int>& a, const pair<string, int>& b) {
+        return a.second > b.second;
+        });
+
+    // 최대 재생수를 가진 장르부터 2개씩 뽑기
+    for (const auto& [genre, count] : v) {
+        auto& vec = um2[genre];
+
+        sort(vec.begin(), vec.end(), [](const Data& a, const Data& b) {
+            if (a.count == b.count) {
+                return a.index < b.index;
+            }
+            return a.count > b.count;
+            });
+
+        for (int i = 0; i < 2 && i < vec.size(); ++i) {
+            answer.push_back(vec[i].index);
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution({ "classic", "pop", "classic", "classic", "pop" },
+        { 500, 600, 150, 800, 2500 });
+}
---
+++ Algorithm/Programmers/탐색/가장 먼 노드.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+// 1번 노드에서 최단 경로로 이동했을 때 간선이 가장 많은 노드들의 개수
+// 간선별 비용 = 1로 동일 -> BFS
+
+int max_dist;
+int answer;
+int MAX = 1e9;
+
+void BFS(const vector<vector<int>>& graph)
+{
+    vector<int> visited(graph.size(), MAX);
+
+    queue<pair<int, int>> q;
+    q.push({ 1, 0 });
+    visited[1] = 0;
+
+    while (!q.empty()) {
+        int node = q.front().first;
+        int dist = q.front().second;
+        q.pop();
+
+        if (dist > max_dist) {
+            // 현재 노드가 최대 거리일경우 갱신
+            max_dist = dist;
+            answer = 1;
+        }
+        else if (dist == max_dist) {
+            // 현재 노드가 이전 최대거리와 같다면 개수만 증가
+            ++answer;
+        }
+
+        int next_dist = dist + 1;
+
+        for (int next : graph[node]) {
+            // 최단거리일경우에만 방문
+            if (next_dist < visited[next]) {
+                visited[next] = next_dist;
+                q.push({ next, next_dist });
+            }
+        }
+    }
+}
+
+int solution(int n, vector<vector<int>> edge) {
+    max_dist = 0;
+    answer = 0;
+
+    vector<vector<int>> graph(n + 1, vector<int>());
+
+    for (const auto& v : edge) {
+        graph[v[0]].push_back(v[1]);
+        graph[v[1]].push_back(v[0]);
+    }
+
+    BFS(graph);
+
+    return answer;
+}
+
+int main()
+{
+    solution(6, { {3, 6} ,{4, 3},{3, 2},{1, 3},{1, 2},{2, 4},{5, 2} });
+}
