2d1c9dd0a860797bc3bc1db853851b9055247d9b
LeeDongHyun <aopo3948@gmail.com>
2026-06-09 10:14:04 +0900

    디스크 컨트롤러, 입국심사
    디스크 컨트롤러
    - 요청 시각으로 한번 정렬한 뒤 시뮬레이션해서 풀이함
    - 이전에 다리를 지나는 트럭 과 비슷한 문제
    
    입국 심사
    - 각 심사대에 사람을 배정하는게 아니라 특정 시간에 몇명을 심사할 수 있는지로 생각해야 하는 문제
    - 특정 시간을 이진 탐색으로 구해서 풀이함
    - 다른 문제에서도 정답이 X 라고 가정했을 때 가능한지 생각해볼 것

---
+++ Algorithm/Programmers/수학/입국심사.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+#include <vector>
+
+using namespace std;
+
+// 빈 심사대로 가서 심사, 더 빨리 끝나는 심사대가 있다면 기다렸다가 받기 가능
+
+// 시간 안에 n명을 모두 처리할 수 있는지 확인
+// 특정 시간을 심사대로 나누면 처리할 수 있는 사람 수가 나옴
+
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+
+    // 최소 = 1, 제일 오래걸리는 시간 = 가장 오래걸리는 심사관 * n
+    long long left = 1;
+    long long right = 1;
+
+    for (int time : times) {
+        right = max(right, (long long)time);
+    }
+    right *= (long long)n;
+
+    while (left <= right) {
+        long long mid = left + (right - left) / 2;
+
+        long long count = 0;
+        for (int time : times) {
+            count += mid / time;
+
+            // 이미 처리 가능하면 빠져나오기
+            if (count >= n) {
+                break;
+            }
+        }
+
+        // 처리 가능하면 구간을 왼쪽으로 이동
+        if (count >= n) {
+            answer = mid;
+            right = mid - 1;
+        }
+        // 처리 불가능하면 구간을 오른쪽으로 이동
+        else {
+            left = mid + 1;
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(6, { 7, 10 });
+}
---
+++ Algorithm/Programmers/자료구조/디스크 컨트롤러.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+// 1. 번호, 요청시각, 소요 시간을 저장하는  대기 큐
+// 2. 소요시간 짧은것 > 요청 시각 빠른 것 > 작업 번호 작은 것 우선순위
+// 3. 작업 시작 시 끝낼때까지
+
+// 종료 시간 - 요청시각 의 평균 구하기
+
+struct S {
+    int request_time;
+    int duration;
+};
+
+struct Compare {
+    bool operator()(const S& a, const S& b) const {
+        if (a.duration == b.duration) {
+            return a.request_time > b.request_time;
+        }
+
+        return a.duration > b.duration;
+    }
+};
+
+int solution(vector<vector<int>> jobs) {
+    int n = jobs.size();
+
+    // 대기 큐
+    priority_queue<S, vector<S>, Compare> pq;
+
+    // 구조체 배열
+    vector<S> data;
+    data.reserve(n);
+
+    // 반환 시간
+    vector<int> t_times;
+    t_times.reserve(n);
+
+    // 시간 순서대로 정렬을 한번 해야함
+    for (const auto& job : jobs) {
+        data.push_back({ job[0], job[1] });
+    }
+    sort(data.begin(), data.end(), [](const S& a, const S& b) {
+        return a.request_time < b.request_time;
+        });
+
+
+    // 현재 시각, 작업 구조체 배열 인덱스, 완성한 작업 개수, 총 시간
+    int current_time = 0;
+    int index = 0;
+    int completed = 0;
+    int total_time = 0;
+
+    // 시뮬레이션
+    while (completed < n) {
+
+        // 현재시각을 보고 요청된 작업 대기큐에 넣음
+        while (index < n && data[index].request_time <= current_time) {
+            pq.push(data[index]);
+            ++index;
+        }
+
+        // 대기큐가 비어있다면 다음 작업 요청 시각으로 점프
+        if (pq.empty()) {
+            current_time = data[index].request_time;
+            continue;
+        }
+
+        // 대기큐가 있다면 꺼내서 작업
+        S job = pq.top();
+        pq.pop();
+
+        current_time += job.duration;
+
+        total_time += current_time - job.request_time;
+
+        ++completed;
+    }
+
+    return total_time / n;
+}
+
+int main()
+{
+    solution({ { 0,3 }, { 1, 9 }, { 3,5 } });
+}
