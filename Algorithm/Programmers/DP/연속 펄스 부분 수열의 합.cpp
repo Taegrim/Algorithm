6014c1aa5dc594d1724794f6b06f30f56426f3bf
LeeDongHyun <aopo3948@gmail.com>
2026-06-10 10:34:29 +0900

    연속 펄스 부분 수열의 합, 풍선 터트리기
    연속 펄스 부분 수열의 합
    - 현재까지 만들수 있는 부분수열의 최대값을 이용하면 되는 문제
    - 현재가 음수곱이라면 이전은 양수곱, 현재가 양수곱이면 이전은 음수곱
    - 이에따라 점화식을 세워서 풀이함
    
    풍선 터트리기
    - 풍선을 터트리기 전에 왼쪽, 오른쪽에서 어떤 풍선이 남는지 봐야함
    - 기회를 안쓸 경우 최솟값이 나오므로 양쪽의 최솟값을 보고 풀이하는 문제

---
+++ Algorithm/Programmers/DP/연속 펄스 부분 수열의 합.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+#include <vector>
+
+using namespace std;
+
+// 연속 부분수열에 펄스 수열을 각 원소끼리 곱함
+// 펄스 수열 : 1, -1이 번갈아 나오는 수열
+
+// 연속 펄스 부분수열의 합 중 가장 큰 것 구하기
+
+long long solution(vector<int> sequence) {
+    // 직전까지의 음수곱, 양수곱의 최대값을 이용하면 될것
+    long long prev_plus = sequence[0];
+    long long prev_minus = -sequence[0];
+
+    long long answer = max(prev_plus, prev_minus);
+
+    for (int i = 1; i < sequence.size(); ++i) {
+        // i번째가 1을 곱해야 하는 경우, i-1은 음수곱
+        //   - prev_minus + sequence[i], sequence[i] 중 큰값
+        long long plus = max(prev_minus + (long long)sequence[i], (long long)sequence[i]);
+
+        // i번째가 -1 을 곱해야 하는 경우, i-1은 양수곱
+        //   - prev_plus - sequence[i],  -sequence[i] 중 큰값
+        long long minus = max(prev_plus - (long long)sequence[i], (long long)-sequence[i]);
+
+        prev_plus = plus;
+        prev_minus = minus;
+
+        answer = max(answer, max(plus, minus));
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution({ 2, 3, -6, 1, 3, -1, 2, 4 });
+}
---
+++ Algorithm/Programmers/그리디/풍선 터트리기.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+#include <vector>
+#include <limits>
+
+using namespace std;
+
+// 임의의 인접한 2개의 풍선 고르고 하나 터트린 다음 밀착
+//  - 번호가 작은 풍선 터트리기는 최대 1번만 (기회 1번)
+
+// 최후까지 남기는게 가능한 풍선 개수 반환
+//  -> 나머지를 전부 터트릴수 있는가
+
+// i번째 풍선의 왼쪽에서 무엇이 남을 수 있는지, 오른쪽에서 무엇이 남을 수 있는지
+// -> 기회를 안쓰면 최솟값이 남음
+
+int solution(vector<int> a) {
+    int answer = 0;
+
+    int n = a.size();
+
+    vector<int> right_min(n);
+    right_min[n - 1] = a[n - 1];
+
+    for (int i = n - 2; i >= 0; --i) {
+        right_min[i] = min(right_min[i + 1], a[i]);
+    }
+
+    int left_min = std::numeric_limits<int>::max();
+
+    for (int i = 0; i < n; ++i) {
+        // 왼쪽 오른쪽에서 하나라도 현재보다 큰게 나오면 됨
+        if (left_min >= a[i] || right_min[i] >= a[i]) {
+            ++answer;
+        }
+
+        left_min = min(left_min, a[i]);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution({ 9,-1,-5 });
+    solution({ -16,27,65,-2,58,-92,-71,-68,-61,-33 });
+}
