5fa9292561cbd36f02c2accb950b44f8d1e3d06c
LeeDongHyun <aopo3948@gmail.com>
2026-06-17 14:48:25 +0900

    110 옮기기
    110 옮기기
    - 110들을 전부 제거하고, 마지막 0 뒤에 붙이는 문제
    - 스택 처리과 비슷하게 110들을 제거하고 이후에 붙이는 방식으로 풀이함

---
+++ Algorithm/Programmers/자료구조/110 옮기기.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+vector<string> solution(vector<string> s) {
+    vector<string> answer;
+    answer.reserve(s.size());
+
+    for (auto& str : s) {
+        int count = 0;
+        string temp = "";
+        temp.reserve(str.size());
+
+        // 110 찾아서 전부 제거
+        for (char c : str) {
+            temp += c;
+
+            int n = temp.size() - 1;
+            if (n >= 2 && temp[n - 2] == '1' && temp[n - 1] == '1' && temp[n] == '0') {
+                for (int i = 0; i < 3; ++i) {
+                    temp.pop_back();
+                }
+                ++count;
+            }
+        }
+
+        // 마지막 0의 위치 찾기
+        int index = -1;
+        for (int i = 0; i < temp.size(); ++i) {
+            if (temp[i] == '0') {
+                index = i;
+            }
+        }
+
+        string num = "";
+        while (count > 0) {
+            num += "110";
+            --count;
+        }
+
+        // 0 뒤에 110들 붙이기
+        temp.insert(index + 1, num);
+
+        answer.push_back(temp);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution({ "1110","100111100","0111111010" });
+}
