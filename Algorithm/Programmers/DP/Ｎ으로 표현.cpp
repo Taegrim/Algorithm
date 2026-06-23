1eea3c5700a787700320ff1b7ff7016c9a34c1d4
Taegrim <aopo3948@gmail.com>
2026-06-23 09:58:27 +0900

    N으로 표현
    - N을 i번 사용했을때 만들수 있는 모든 수의 집합을 기록함
    - 좌항식, 우항식을 각각 N을 left,right번 사용했을때 만들수 있는 수로 분리함
    - 이런식으로 N을 8번사용했을때까지 나오는 수를 구하는 방식으로 풀이

---
+++ Algorithm/Programmers/DP/Ｎ으로 표현.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+
+int solution(int N, int number) {
+    int answer = -1;
+    vector<unordered_set<int>> dp(9);
+
+    // N을 이어붙이는 용도
+    int paste = 0;
+
+    for (int i = 1; i <= 8; ++i)
+    {
+        paste = paste * 10 + N;
+        dp[i].insert(paste);
+
+        // 좌항식에 N을 left개, 우항식에 N을 Right개 사용했을 때
+        // 나올 수 있는 모든 경우를 dp[i]에 저장
+        for (int left = 1; left < i; ++left)
+        {
+            int right = i - left;
+
+            for (int a : dp[left])
+            {
+                for (int b : dp[right])
+                {
+                    dp[i].insert(a + b);
+                    dp[i].insert(a - b);
+                    dp[i].insert(a * b);
+
+                    // 0 나누기 방지
+                    if (b != 0)
+                    {
+                        dp[i].insert(a / b);
+                    }
+                }
+            }
+        }
+
+        if (dp[i].count(number))
+        {
+            answer = i;
+            break;
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(5, 12);
+    solution(2, 11);
+}
