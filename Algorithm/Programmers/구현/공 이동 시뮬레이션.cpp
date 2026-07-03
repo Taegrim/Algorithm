66772be8fc1a9f05256671f6acd0761d5e1b1c35
Taegrim <aopo3948@gmail.com>
2026-07-03 09:41:44 +0900

    공 이동 시뮬레이션, 억억단을 외우자
    공 이동 시뮬레이션
    - 역방향으로 시뮬레이션, 벽에 붙어있는지 여부에 따라 체크하는 방식으로 풀이
    
    억억단을 외우자
    - 약수 개수를 이용해서 풀이하는 문제
    - 전처리 + DP 를 이용해서 풀이함

---
+++ Algorithm/Programmers/구현/공 이동 시뮬레이션.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n, int m, int x, int y, vector<vector<int>> queries) {
+    // 범위 구간 지정
+    long long top = x;
+    long long down = x;
+    long long left = y;
+    long long right = y;
+
+    // 역방향 시뮬레이션
+    // 현재 위치가 벽과 닿아있다면 거리 내에 아무 위치에서 올 수 있음
+    //  -> 이동할 수 있는 구간으로 표현
+    for (int i = queries.size() - 1; i >= 0; --i)
+    {
+        long long dx = queries[i][1];
+
+        if (queries[i][0] == 0)
+        {
+            // 왼쪽으로 이동했었음
+            // 벽에 안붙어있다면 정확히 dx 거리에서 와야함
+            if (left != 0)
+            {
+                left += dx;
+            }
+            right = min(right + dx, (long long)(m - 1));
+        }
+        else if (queries[i][0] == 1)
+        {
+            // 오른쪽으로 이동했었음
+            if (right != m - 1)
+            {
+                right -= dx;
+            }
+            left = max(left - dx, 0LL);
+        }
+        else if (queries[i][0] == 2)
+        {
+            // 위로 이동했었음
+            if (top != 0)
+            {
+                top += dx;
+            }
+            down = min(down + dx, (long long)(n - 1));
+        }
+        else if (queries[i][0] == 3)
+        {
+            // 아래로 이동했었음
+            if (down != n - 1)
+            {
+                down -= dx;
+            }
+            top = max(top - dx, 0LL);
+        }
+
+        // 이전 지점이 범위를 벗어나면 도착할 수 없음
+        if (left > right || top > down)
+        {
+            return 0;
+        }
+    }
+
+    return (right - left + 1) * (down - top + 1);
+}
+
+int main()
+{
+    solution(2, 2, 0, 0, {{2, 1}, {0, 1}, {1, 1}, {0, 1}, {2, 1}} );
+    solution(2, 5, 0, 1, {{3, 1}, {2, 2}, {1, 1}, {2, 3}, {0, 1}, {2, 1}} );
+}
---
+++ Algorithm/Programmers/수학/억억단을 외우자.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// s ~ e 사이의 수 중 억억단에서 가장 많이 등장한 수
+//  -> 약수 개수?
+
+vector<int> solution(int e, vector<int> starts) {
+    vector<int> answer;
+
+    vector<int> counts(e + 1, 0);
+
+    // 약수 개수 전처리, 에라토네스 체
+    for (int i = 1; i <= e; ++i)
+    {
+        for (int j = i; j <= e; j += i)
+        {
+            ++counts[j];
+        }
+    }
+
+    // 역순으로 검사해서 약수가 가장 많은 수 저장
+    vector<int> dp(e + 1, 0);
+    int count = counts[e];  // 약수 가장 많은 개수
+    dp[e] = e;
+
+    for (int i = e - 1; i >= 1; --i)
+    {
+        // 현재 숫자가 약수가 더 많거나 같다면 dp 현재 수로
+        if (count <= counts[i])
+        {
+            count = counts[i];
+            dp[i] = i;
+        }
+        // 현재 숫자가 약수가 더 적다면 이전 dp 값 그대로
+        else
+        {
+            dp[i] = dp[i + 1];
+        }
+    }
+
+    for (int start : starts)
+    {
+        answer.push_back(dp[start]);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(8, { 1,3,7 });
+}
