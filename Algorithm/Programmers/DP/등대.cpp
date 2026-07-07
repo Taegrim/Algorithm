a4f5590f4ae3d2cab97b96ff40ebd7e39f1eed9a
Taegrim <aopo3948@gmail.com>
2026-07-07 09:37:11 +0900

    금과 은 운반하기, 등대
    금과 은 운반하기
    - 입국심사와 비슷한 문제
    - 현재 시간을 기준으로 운반 가능한지 확인해서 시간을 이분탐색함
    
    등대
    - 매출하락 최소화와 비슷한 문제
    - 현재 노드를 키는지, 끄는지에 따라 필요한 최소 개수를 dp로 저장하면서 루트까지 올라가는 방식

---
+++ Algorithm/Programmers/DP/등대.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> graph;
+vector<vector<int>> dp;
+
+void DFS(int current, int parent)
+{
+    dp[current][0] = 0; // 현재 등대를 끔
+    dp[current][1] = 1; // 현재 등대를 켬
+
+    for (int next : graph[current])
+    {
+        if (next == parent) continue;
+
+        DFS(next, current);
+
+        // 현재 등대가 꺼져있으면 자식들을 전부 켜야함
+        dp[current][0] += dp[next][1];
+
+        // 현재 등대가 켜져있으면 자식들은 킨것, 끈것 중 최솟값 선택
+        dp[current][1] += min(dp[next][0], dp[next][1]);
+    }
+}
+
+int solution(int n, vector<vector<int>> lighthouse) {
+    // dp : i번째 등대가 꺼짐(0), 켜짐(1) 일때 서브트리에서 필요한 최소 등대 개수
+    //  i번째 등대를 켜면 자식에선 켜도되고, 안켜도 됨
+    //  dp[i][1] = 1 + min(dp[child][0], dp[child][1]) ...
+    //  dp[i][0] = dp[child][1] ...
+
+    graph.assign(n + 1, vector<int>());
+    dp.assign(n + 1, vector<int>(2, 0));
+
+    for (const auto& house : lighthouse)
+    {
+        graph[house[0]].push_back(house[1]);
+        graph[house[1]].push_back(house[0]);
+    }
+
+    DFS(1, 1);
+
+    return min(dp[1][0], dp[1][1]);
+}
+
+int main()
+{
+    solution(8, {{1, 2}, {1, 3}, {1, 4}, {1, 5}, {5, 6}, {5, 7}, {5, 8}} );
+    solution(10, {{4, 1}, {5, 1}, {5, 6}, {7, 6}, {1, 2}, {1, 3}, {6, 8}, {2, 9}, {9, 10}} );
+}
---
+++ Algorithm/Programmers/수학/금과 은 운반하기.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+
+#include <vector>
+
+using namespace std;
+
+// i번 도시 : 금 g{i}, 은 s{i}, 편도 시간 t{i}, 최대 운반 w{i}
+
+// 여러 도시에서 동시에 운반 가능
+
+long long solution(int a, int b, vector<int> g, vector<int> s, vector<int> w, vector<int> t) {
+    long long answer = -1;
+
+    int n = g.size();
+
+    long long left = 1;
+    long long right = 1e16;
+
+    while (left <= right)
+    {
+        long long current = (right - left) / 2 + left;
+
+        // 총 금괴, 총 은괴, 모든 광물의 무게
+        long long gold = 0;
+        long long silver = 0;
+        long long total = 0;
+
+        for (int i = 0; i < n; ++i)
+        {
+            long long time = t[i];
+            long long weight = w[i];
+
+            // 현재 시간동안 i도시에선 몇번 운반 가능한지
+            // 몇번 왕복 가능한지 검사하고, 남은 시간이 편도 시간 이상이면 횟수 증가
+            long long count = current / (time * 2LL);
+            if (current % (time * 2LL) >= time)
+            {
+                ++count;
+            }
+
+            // i 도시에서 운반 가능한 최대 무게
+            long long max_weight = count * weight;
+
+            gold += min((long long)g[i], max_weight);
+            silver += min((long long)s[i], max_weight);
+            total += min((long long)g[i] + (long long)s[i], max_weight);
+        }
+
+        // 금괴, 은괴, 광물의 합이 요구한대로 올 수 있는지 확인해서 구간 설정
+        if (gold >= a && silver >= b && total >= a + b)
+        {
+            answer = current;
+            right = current - 1;
+        }
+        else
+        {
+            left = current + 1;
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(10, 10, { 100 }, { 100 }, { 7 }, { 10 });
+    solution(90, 500, { 70, 70, 0 }, { 0, 0, 500 }, { 100, 100, 2 }, { 4, 8, 1 });
+}
