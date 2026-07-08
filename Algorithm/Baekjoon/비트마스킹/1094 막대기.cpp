049010e6ce6a616583e2caac28f543bcb5cab0de
Taegrim <aopo3948@gmail.com>
2026-07-08 12:38:16 +0900

    백준 1094 막대기, 1987 알파벳
    비트마스킹 연습

---
+++ Algorithm/Baekjoon/비트마스킹/1094 막대기.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int target;
+    cin >> target;
+
+    int count = 0;
+    while (target > 0)
+    {
+        if (target & 1)
+        {
+            ++count;
+        }
+        target >>= 1;
+    }
+
+    cout << count << "\n";
+}
---
+++ Algorithm/Baekjoon/비트마스킹/1987 알파벳.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<char>> graph;
+int R, C;
+int answer = 0;
+
+int dr[]{ -1, 0, 1, 0 };
+int dc[]{ 0, 1, 0, -1 };
+
+void DFS(int row, int col, int route, int depth)
+{
+    answer = max(answer, depth);
+
+    for (int i = 0; i < 4; ++i)
+    {
+        int nr = row + dr[i];
+        int nc = col + dc[i];
+
+        if (nr < 0 || nc < 0 || nr >= R || nc >= C) continue;
+
+        int next = 1 << (graph[nr][nc] - 'A');
+        if (route & next) continue;
+
+        DFS(nr, nc, route | next, depth + 1);
+    }
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> R >> C;
+
+    graph.assign(R, vector<char>(C));
+
+    for (int i = 0; i < R; ++i)
+    {
+        for (int j = 0; j < C; ++j)
+        {
+            cin >> graph[i][j];
+        }
+    }
+
+    int bit = 1 << (graph[0][0] - 'A');
+    DFS(0, 0, bit, 1);
+
+    cout << answer << "\n";
+}
