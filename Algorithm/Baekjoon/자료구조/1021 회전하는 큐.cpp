74fcf4da421032b421d21e907709fccc70fda1c8
Taegrim <aopo3948@gmail.com>
2026-07-22 09:23:19 +0900

    회전하는 큐
    deque를 이용, 왼쪽or오른쪽 회전인지를 구해서 처리함

---
+++ Algorithm/Baekjoon/자료구조/1021 회전하는 큐.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    deque<int> deq(n);
+    for (int i = 1; i <= n; ++i)
+    {
+        deq[i - 1] = i;
+    }
+
+    int count = 0;
+    for (int i = 0; i < m; ++i)
+    {
+        int num;
+        cin >> num;
+
+        // 몇번째 위치에 있는지 찾기
+        int idx = -1;
+        int size = deq.size();
+        for (int j = 0; j < size; ++j)
+        {
+            if (deq[j] == num)
+            {
+                idx = j;
+                break;
+            }
+        }
+
+        // 회전
+        while (!deq.empty() && deq.front() != num)
+        {
+            int temp = -1;
+            if (idx * 2 + 1 <= size)
+            {
+                // 왼쪽 회전값 저장
+                temp = deq.front();
+                deq.pop_front();
+                deq.push_back(temp);
+            }
+            else
+            {
+                // 오른쪽 회전값 저장
+                temp = deq.back();
+                deq.pop_back();
+                deq.push_front(temp);
+            }
+            ++count;
+        }
+        deq.pop_front();
+    }
+
+    cout << count << "\n";
+}
