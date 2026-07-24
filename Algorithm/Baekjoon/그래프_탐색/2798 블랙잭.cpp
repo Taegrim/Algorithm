6d19eee77d275e490e5788e87fd2ad9ef80414e3
Taegrim <aopo3948@gmail.com>
2026-07-24 09:57:54 +0900

    블랙잭
    백트래킹으로 조합 구하는 방식으로 풀이

---
+++ Algorithm/Baekjoon/그래프_탐색/2798 블랙잭.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> v;
+int answer;
+int n, m;
+
+void DFS(int idx, int count, int sum)
+{
+    if (count == 3)
+    {
+        answer = max(answer, sum);
+        return;
+    }
+
+    for (int i = idx; i < n; ++i)
+    {
+        if (sum + v[i] > m) continue;
+
+        DFS(i + 1, count + 1, sum + v[i]);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+
+    v.assign(n, 0);
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> v[i];
+    }
+
+    DFS(0, 0, 0);
+
+    cout << answer << "\n";
+}
