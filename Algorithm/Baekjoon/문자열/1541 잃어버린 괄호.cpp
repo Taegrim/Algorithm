826d02030c1b34eed5a4ba41ec2574dc48d2e955
Taegrim <aopo3948@gmail.com>
2026-07-27 09:44:23 +0900

    잃어버린 괄호, 분해합
    잃어버린 괄호
    - 문자열을 읽어가며 -, + 에 따라 다르게 처리하는 방식으로 풀이함
    
    분해합
    - 브루트포스로 풀이함

---
+++ Algorithm/Baekjoon/문자열/1541 잃어버린 괄호.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int start = 0;
+    int sum = 0;
+
+    vector<int> v;
+
+    // 부호나 끝을 만나면 직전 숫자를 합치기
+    // +를 만나면 계속 합치고
+    // -를 만나면 여태까지 합친 숫자를 벡터에 넣음
+    for (int i = 0; i <= s.size(); ++i)
+    {
+        if (i == s.size() || s[i] == '+' || s[i] == '-')
+        {
+            sum += stoi(s.substr(start, i - start));
+
+            if (i == s.size() || s[i] == '-')
+            {
+                v.push_back(sum);
+                sum = 0;
+            }
+
+            start = i + 1;
+        }
+    }
+
+    int ret = v.front();
+    for (int i = 1; i < v.size(); ++i)
+    {
+        ret -= v[i];
+    }
+    cout << ret << "\n";
+}
---
+++ Algorithm/Baekjoon/브루트포스/2231 분해합.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int base = n;
+    int digit = 0;
+    while (base > 0)
+    {
+        ++digit;
+        base /= 10;
+    }
+
+    // 자릿수 * 9 를 뺀 수부터 시작
+    int start = max(1, n - digit * 9);
+
+    int ret = 0;
+    for (int i = start; i <= n; ++i)
+    {
+        long long sum = i;
+        long long number = i;
+
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        if (sum == n)
+        {
+            ret = i;
+            break;
+        }
+    }
+    cout << ret << "\n";
+}
