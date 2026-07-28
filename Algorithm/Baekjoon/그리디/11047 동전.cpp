8ab77a4ef37398f40a425c84af2006c0f1de349b
Taegrim <aopo3948@gmail.com>
2026-07-28 10:03:45 +0900

    백준 11047 동전
    오름차순 정렬 + 배수 동전이라 그리디 사용 가능

---
+++ Algorithm/Baekjoon/그리디/11047 동전.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> v(n);
+    
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> v[i];
+    }
+
+    int count = 0;
+    long long sum = 0;
+
+    for (int i = n - 1; i >= 0; --i)
+    {
+        if (sum == k) break;
+
+        int diff = k - sum;
+        int div = diff / v[i];
+
+        sum += div * v[i];
+        count += div;
+    }
+
+    cout << count << "\n";
+}
