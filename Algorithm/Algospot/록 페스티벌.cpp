c71637b68086d32f1abfb1ae0e4173d73a8ba2bd
Taegrim <aopo3948@gmail.com>
2026-08-03 10:59:46 +0900

    록 페스티벌 문제 풀이, 파일 분류
    록 페스티벌
     - 슬라이딩 윈도우로 풀이하는 문제
     - 섭외한 팀 수부터 n까지 길이를 늘려가면서 슬라이딩 윈도우 진행

---
+++ Algorithm/Algospot/록 페스티벌.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <numeric>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+// 하루 한팀 밴드가 공연
+// L팀은 섭외, L일 이상 진행
+// 공연장 하루 빌리는 비용 매일ㄷ ㅏ름
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int c;
+    cin >> c;
+
+    while (c--)
+    {
+        // 대여할 수 있는 날들의 수, 공연 팀 수
+        int n, l;
+        cin >> n >> l;
+
+        vector<int> v(n);
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> v[i];
+        }
+
+        double cost = numeric_limits<double>::max();
+
+        // 길이만큼 슬라이딩 윈도우
+        for (int len = l; len <= n; ++len)
+        {
+            long long sum = 0;
+
+            // 처음 len 만큼 길이 설정
+            for(int i = 0; i < len; ++i)
+            {
+                sum += v[i];
+            }
+            cost = min(cost, (double)sum / len);
+
+            // 슬라이딩 윈도우
+            for (int i = len; i < n; ++i)
+            {
+                sum -= v[i - len]; // 맨앞 빼기
+                sum += v[i];       // 맨뒤 넣기
+
+                cost = min(cost, (double)sum / len);
+            }
+        }
+
+        cout << fixed << setprecision(10) << cost << '\n';
+    }
+}
