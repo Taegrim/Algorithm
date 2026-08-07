022f7e42b2753d68be5b92f607659a55bae27bfe
Taegrim <aopo3948@gmail.com>
2026-08-07 20:00:07 +0900

    DP 문제 풀이
    01배낭, 동전교환 DP 문제 풀이

---
+++ Algorithm/Baekjoon/DP/12865 평범한 배낭.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<pair<int, int>> v;   // 무게, 가치
+vector<vector<int>> dp;
+
+int n, k;
+
+int Func(int idx, int weight)
+{
+    // 탐색 끝
+    if (idx < 0)
+    {
+        return 0;
+    }
+
+    if (dp[idx][weight] != -1)
+    {
+        return dp[idx][weight];
+    }
+
+    int w = v[idx].first;
+    int val = v[idx].second;
+
+    int& ret = dp[idx][weight];
+
+    // 현재 물건을 넣지 않는 경우, 이전값 그대로 사용
+    ret = Func(idx - 1, weight);
+
+    // 현재 물건을 넣는 경우
+    if (weight - w >= 0)
+    {
+        ret = max(ret, Func(idx - 1, weight - w) + val);
+    }
+
+    return ret;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> k;
+
+    v.resize(n);
+    dp.assign(n + 1, vector<int>(k + 1, -1));
+
+    for (auto& [weight, value] : v)
+    {
+        cin >> weight >> value;
+    }
+
+    cout << Func(n - 1, k) << "\n";
+}
---
+++ Algorithm/LeetCode/DP/동전교환.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> coins;
+vector<int> dp;
+
+constexpr int MAX = 1e9;
+
+int Topdown(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+
+    if (num < 0)
+    {
+        return MAX;
+    }
+
+    // 이미 있다면 
+    if (dp[num] != -1)
+    {
+        return dp[num];
+    }
+
+    int ret = MAX;
+
+    for (int coin : coins)
+    {
+        int prev = Topdown(num - coin);
+
+        if (prev != MAX)
+        {
+            ret = min(ret, prev + 1);
+        }
+    }
+
+    return dp[num] = ret;
+}
+
+int Bottomup(int num)
+{
+    vector<int> dp(num + 1, MAX);
+
+    dp[0] = 0;
+
+    for (int i = 1; i <= num; ++i)
+    {
+        for (int coin : coins)
+        {
+            if (i - coin < 0) continue;
+            if (dp[i - coin] == MAX) continue;
+
+            dp[i] = min(dp[i], dp[i - coin] + 1);
+        }
+    }
+
+    return dp[num];
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    coins.assign(m, 0);
+    dp.assign(n + 1, -1);
+
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> coins[i];
+    }
+
+    int topdown = Topdown(n);
+    int bottomup = Bottomup(n);
+
+    cout << (topdown == MAX ? -1 : topdown) << '\n';
+    cout << (bottomup == MAX ? -1 : bottomup) << '\n';
+}
