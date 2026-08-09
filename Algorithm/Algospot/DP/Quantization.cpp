999f9c74df627b68be4414bec2f758336e2d2cdb
Taegrim <aopo3948@gmail.com>
2026-08-09 20:21:34 +0900

    원주율 외우기, 양자화 문제
    0부터 시작해서 몇개씩 끊어서 하나의 구간으로 처리할지 판별하는 DP 문제

---
+++ Algorithm/Algospot/DP/Quantization.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// s개 숫자만 이용해서 양자화
+// 숫자별 오차 제곱의 합이 최소화 되도록
+
+vector<int> v;
+vector<int> sum;
+vector<int> square_sum;
+vector<vector<int>> dp;
+int n, s;
+
+constexpr int MAX = 1e9;
+
+// (v[i] - 평균)^2 의 합
+// v[i]*2 - 2 * 평균 * v[i] + 평균^2  의 합
+// 제곱의 부분합 - 2*평균*구간합 + 평균^2 * 구간 길이
+int GetMin(int left, int right)
+{
+    // 평균값이 최소오차
+
+    // 구간합, 제곱합
+    int num = sum[right] - (left == 0 ? 0 : sum[left - 1]);
+    int square_num = square_sum[right] - (left == 0 ? 0 : square_sum[left - 1]);
+
+    int len = right - left + 1;
+
+    // 평균값
+    int val = static_cast<int>(0.5 + (double)num / len);
+
+    return square_num - 2 * val * num + val * val * len;
+}
+
+int F(int start, int remain)
+{
+    if (start == n)
+    {
+        return 0;
+    }
+
+    // 숫자가 남았지만 구간을 더 나눌수가 없다면 불가능한 것
+    if (remain == 0)
+    {
+        return MAX;
+    }
+
+    int& ret = dp[start][remain];
+    if (ret != -1)
+    {
+        return ret;
+    }
+
+    ret = MAX;
+    for (int len = 1; len + start <= n; ++len)
+    {
+        ret = min(ret, GetMin(start, start + len - 1) + F(start + len, remain - 1));
+    }
+
+    return ret;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> n >> s;
+
+        v.assign(n, 0);
+        dp.assign(n + 1, vector<int>(s + 1, -1));
+        sum.assign(n, 0);
+        square_sum.assign(n, 0);
+
+        for (int& num : v)
+        {
+            cin >> num;
+        }
+
+        sort(v.begin(), v.end());
+
+        sum[0] = v[0];
+        square_sum[0] = v[0] * v[0];
+
+        for (int i = 1; i < n; ++i)
+        {
+            sum[i] = sum[i - 1] + v[i];
+            square_sum[i] = square_sum[i - 1] + v[i] * v[i];
+        }
+
+        cout << F(0, s) << "\n";
+    }
+
+}
---
+++ Algorithm/Algospot/DP/원주율 외우기.cpp
@@ -0,0 +1,120 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 숫자를 3~5자리로 끊어서 읽으면서 난이도 최소화
+
+vector<int> dp;
+string num;
+
+// 시작 index, 난이도를 구할 글자수
+int GetLevel(int start, int count)
+{
+    string str = num.substr(start, count);
+
+    char prev = str[0];
+    int prev_diff = str[1] - str[0];
+
+    bool is_equal = true;       // 모든 숫자 같음
+    bool is_step = true;        // 등차수열
+    bool is_step_one = true;    // 등차 & 단조
+    bool is_altenate = true;    // 번갈아
+
+    for (int i = 1; i < count; ++i)
+    {
+        // 1. 모든 숫자가 같은지
+        if (str[i] != prev)
+        {
+            is_equal = false;
+        }
+
+        // 2. 등차수열인지, 차이가 이전과 다르면 등차수열 X
+        if (prev_diff != str[i] - prev)
+        {
+            is_step = false;
+        }
+
+        // 3. 등차수열이면서 단조인지
+        if (abs(str[i] - prev) != 1)
+        {
+            is_step_one = false;
+        }
+
+        // 4. 번갈아 나오는지, i가 i%2와 다르면 번갈아 X
+        if (str[i] != str[i % 2])
+        {
+            is_altenate = false;
+        }
+
+        prev_diff = str[i] - prev;
+        prev = str[i];
+    }
+
+    if (is_equal)
+    {
+        return 1;
+    }
+
+    if (is_step)
+    {
+        if (is_step_one)     // 등차면서 단조면
+        {
+            return 2;
+        }
+
+        return 5;   // 등차수열이면
+    }
+
+    if (is_altenate)
+    {
+        return 4;   // 번갈아 나오면
+    }
+
+    return 10;
+}
+
+int F(int start)
+{
+    // 문자열의 끝에 도달하면 종료
+    if (start == num.size())
+    {
+        return 0;
+    }
+
+    int& ret = dp[start];
+    if (ret != -1)
+    {
+        return ret;
+    }
+
+    ret = 1e9;
+    for (int count = 3; count <= 5; ++count)
+    {
+        if (start + count > num.size()) continue;
+
+        ret = min(ret, F(start + count) + GetLevel(start, count));
+    }
+
+    return ret;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    //cin >> num;
+    //cout << GetLevel(0, 4);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> num;
+        dp.assign(num.size() + 1, -1);
+
+        cout << F(0) << "\n";
+    }
+}
