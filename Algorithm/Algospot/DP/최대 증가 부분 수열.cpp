0cd425e13c3fca2e1439b32868c1b4a78211da5c
Taegrim <aopo3948@gmail.com>
2026-08-09 15:03:46 +0900

    합친 LIS 풀이 전 LIS 탑다운 방식 복습
    합친 LIS 풀이 전 LIS 탑다운 방식 풀이 복습

--- Algorithm/Algospot/DP/최대 증가 부분 수열.cpp
+++ Algorithm/Algospot/DP/최대 증가 부분 수열.cpp
@@ -3,7 +3,62 @@
 
 using namespace std;
 
-vector<int> dp;
+vector<int> v;
+int n;
+
+int BottomUp()
+{
+    vector<int> dp(n, 1);   // BottomUp DP
+
+    int ret = 1;
+    for (int i = 1; i < n; ++i)
+    {
+        //i 에 대해서 이전 작은 숫자 확인
+        // 가장 큰 값 + 1
+
+        for (int j = 0; j < i; ++j)
+        {
+            if (v[i] > v[j])
+            {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        ret = max(ret, dp[i]);
+    }
+
+    return ret;
+}
+
+vector<int> dp; // TopDown DP
+
+int GetLIS(int idx)
+{
+    int& ret = dp[idx];
+    if (ret != -1)
+    {
+        return ret;
+    }
+
+    ret = 1;
+    for (int i = idx + 1; i < n; ++i)
+    {
+        if (v[idx] >= v[i]) continue;
+
+        ret = max(ret, GetLIS(i) + 1);
+    }
+
+    return ret;
+}
+
+int TopDown()
+{
+    int ret = 1;
+    for (int i = 0; i < n; ++i)
+    {
+        ret = max(ret, GetLIS(i));
+    }
+    return ret;
+}
 
 int main()
 {
@@ -15,33 +70,18 @@ int main()
 
     while(t--)
     {
-        int n;
         cin >> n;
 
-        vector<int> v(n);
-        dp.assign(n, 1);
+        v.assign(n, 0);     // 수열 초기화
+        dp.assign(n, -1);    // 탑다운 DP 초기화
 
         for (int& i : v)
         {
             cin >> i;
         }
 
-        int ret = 1;
-        for (int i = 1; i < n; ++i)
-        {
-            //i 에 대해서 이전 작은 숫자 확인
-            // 가장 큰 값 + 1
+        cout << BottomUp() << "\n";
+        cout << TopDown() << "\n";
 
-            for (int j = 0; j < i; ++j)
-            {
-                if (v[i] > v[j])
-                {
-                    dp[i] = max(dp[i], dp[j] + 1);
-                }
-            }
-            ret = max(ret, dp[i]);
-        }
-
-        cout << ret << "\n";
     }
 }
