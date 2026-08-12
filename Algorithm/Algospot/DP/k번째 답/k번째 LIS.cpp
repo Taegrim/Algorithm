21f9a3a8ade7a148716ff4a2b8e7447cb31cfb1d
Taegrim <aopo3948@gmail.com>
2026-08-12 11:15:51 +0900

    알고스팟 DP k번째 답 문제
    KLIS, 모스부호 사전 풀이
    
    1. 완전탐색 + 메모이제이션으로 문제를 푼다
    2. 답의 수를 세는 문제를 푼다. (가능하면 재귀, 불가능하면 건너뛰기)

---
+++ Algorithm/Algospot/DP/k번째 답/k번째 LIS.cpp
@@ -0,0 +1,134 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> v;
+vector<int> dp_len;
+vector<int> dp_count;
+int n, k;
+
+constexpr long long MAX = 2000000000;
+
+// idx 뒤로 추가할 수 있는 LIS 개수
+int LIS(int idx)
+{
+    int& ret = dp_len[idx + 1];
+    if (ret != -1)
+    {
+        return ret;
+    }
+
+    ret = 0;
+    for (int next = idx + 1; next < n; ++next)
+    {
+        if (idx == -1 || v[idx] < v[next])
+        {
+            ret = max(ret, LIS(next) + 1);
+        }
+    }
+
+    return ret;
+}
+
+// idx 이후로 만들 수 있는 LIS 개수
+int Count(int idx)
+{
+    // LIS 길이가 0이면 1 반환
+    // 더이상 선택 불가능하면 LIS 하나 완성된 것
+    if (LIS(idx) == 0)
+    {
+        return 1;
+    }
+
+    int& ret = dp_count[idx + 1];
+    if (ret != -1)
+    {
+        return ret;
+    }
+
+    ret = 0;
+    for (int next = idx + 1; next < n; ++next)
+    {
+        // 현재 LIS가 다음 방문할곳의 LIS + 1 과 같다면
+        //  -> 방문할 곳의 LIS가 최대라면
+        if ((idx == -1 || v[idx] < v[next])
+            && LIS(idx) == LIS(next) + 1)
+        {
+            ret = min<long long>(MAX, (long long)ret + (long long)Count(next));
+        }
+    }
+    return ret;
+}
+
+void ReConstruct(int idx, int skip, vector<int>& lis)
+{
+    // 시작점은 포함하고 시작
+    if (idx != -1)
+    {
+        lis.push_back(v[idx]);
+    }
+
+    // LIS에서 idx의 후보로 next1, next2 가 있을때,
+    //  v[next1] < v[next2] 라면 next1은 next2와 연결할 수 있고, next의 LIS를 그대로 사용할 수 있으므로
+    //  둘이 같아질 수 없다.
+    //  -> 후보가 여러개라면 반드시 v[next1] > v[next2] 여야한다,
+    //     즉 내림차순으로 있어야 둘이 같은 길이를 가질 수 있다
+    //     따라서 뒤에서부터 확인하면 사전순인걸 확인 가능함
+
+    for (int next = n - 1; next > idx; --next)
+    {
+        if ((idx == -1 || v[idx] < v[next])
+            && LIS(idx) == LIS(next) + 1)
+        {
+            int count = Count(next);
+
+            if (count <= skip)
+            {
+                // 이 숫자로 시작하는 LIS 전부 건너뜀
+                skip -= count;
+            }
+            else
+            {
+                // 다음 숫자가 v[next]라 확정하고 확인
+                // 찾는 k번째 LIS가 이 묶음에 있음
+                ReConstruct(next, skip, lis);
+                break;
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> n >> k;
+
+        v.assign(n, 0);
+        dp_len.assign(n + 1, -1);
+        dp_count.assign(n + 1, -1);
+
+        for (int& i : v)
+        {
+            cin >> i;
+        }
+
+        cout << LIS(-1) << "\n";
+
+        vector<int> lis;
+        ReConstruct(-1, k - 1, lis);
+        for (int i : lis)
+        {
+            cout << i << " ";
+        }
+        cout << "\n";
+    }
+}
---
+++ Algorithm/Algospot/DP/k번째 답/모스부호사전.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+// 현재 맨 앞글자로 '-' 를 배치했을때 만들수 있는 경우의 수
+//  -> C(n-1 + m, m),  전체 남은 부호 개수에서 장점을 배치할 수 있는 경우의 수
+//     경우의 수가 현재 남은 k보다 작다면 만들수없으므로 'o'를 배치하고 다음 글자로 이동
+
+vector<vector<int>> dp; // dp[n][m] 은 n,m을 이용해 만들 수 있는 경우의 수
+vector<vector<int>> bino;   // bino[n][m] 은 C(n, m), 조합의 수
+int n, m;
+
+constexpr int MAX = 1000000003;
+
+// n, m으로 만들 수 있는 경우의 수
+// 현재 자리에 n 배치/ m 배치이므로 Count(n, m) = Count(n - 1, m) + Count(n, m - 1)
+int Count(int n, int m)
+{
+    if (n == 0 || m == 0)
+    {
+        return 1;
+    }
+
+    int& ret = dp[n][m];
+    if (ret != -1)
+    {
+        return ret;
+    }
+
+    return ret = min(MAX, Count(n - 1, m) + Count(n, m - 1));
+}
+
+// 이항계수로 미리 전처리 하는 방식
+void MakeBino()
+{
+    bino.assign(201, vector<int>(201, 0));
+
+    for (int i = 0; i <= 200; ++i)
+    {
+        bino[i][0] = bino[i][i] = 1;
+
+        for (int j = 1; j < i; ++j)
+        {
+            bino[i][j] = min(MAX, bino[i - 1][j - 1] + bino[i - 1][j]);
+        }
+    }
+}
+
+// 현재 남은 장점, 단점 개수
+// 이후 k번째 숫자가 정답 (k는 재귀할때마다 감소)
+string Func(int n, int m, int k)
+{
+    // 장점이 없다면 나머진 전부 단점
+    if (n == 0)
+    {
+        return string(m, 'o');
+    }
+
+    // 단점이 없다면 나머진 전부 장점
+    if (m == 0)
+    {
+        return string(n, '-');
+    }
+
+    //// '-' 를 배치한다 가정했을때 만들수있는 조합의 수 계산, DP탑다운
+    //int skip = Count(n - 1, m);
+
+    // 전처리한 이항계수 그대로 사용
+    // C(n + m, n) -> 전체 n+m 개의 위치 중 n개를 배치하는 조합
+    int skip = bino[n + m - 1][n - 1];
+
+    if (skip >= k)
+    {
+        // 만들수있는 경우 장점 '-' 배치
+        return '-' + Func(n - 1, m, k);
+    }
+    else
+    {
+        // 만들수없다면 단점 'o' 배치하고 통째로 건너뜀
+        return 'o' + Func(n, m - 1, k - skip);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    // 이항계수 전처리
+    MakeBino();
+
+    while (t--)
+    {
+        int k;
+        // n개 장점 -, m개 단점 o, 사전순 k번째는?
+        cin >> n >> m >> k;
+
+        //// 탑다운 처리시 DP 확보
+        dp.assign(n + 1, vector<int>(m + 1, -1));
+
+        cout << Func(n, m, k) << "\n";
+    }
+}
