376fbf198f25f22c4e86ef54eaf986fd91a3609c
Taegrim <aopo3948@gmail.com>
2026-08-13 09:51:58 +0900

    알고스팟 DP 문제
    실험 데이터 복구하기, 웨브바짐
    외판원 문제와 같이 사용했던 것들을 비트마스킹으로 처리해야 함

---
+++ Algorithm/Algospot/DP/실험 데이터 복구하기.cpp
@@ -0,0 +1,187 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+constexpr int MAX_N = 15;
+int n;
+string word[MAX_N];
+int dp[MAX_N][1 << MAX_N], overlap[MAX_N][MAX_N];
+
+
+int GetOverlap(const string& a, const string& b)
+{
+    int max_len = min(a.size(), b.size());
+
+    for (int len = max_len; len > 0; --len)
+    {
+        bool same = true;
+
+        for (int i = 0; i < len; ++i)
+        {
+            if (a[a.size() - len + i] != b[i])
+            {
+                same = false;
+                break;
+            }
+        }
+
+        if (same)
+        {
+            return len;
+        }
+    }
+
+    return 0;
+}
+
+void PreCalc()
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            if (i != j)
+            {
+                overlap[i][j] = GetOverlap(word[i], word[j]);
+            }
+        }
+    }
+}
+
+// 마지막에 붙인 문자열, 사용한 문자열이 주어질때
+// 앞으로 구할 수 있는 최대 겹침 길이
+int Restore(int last, int used)
+{
+    // 전부 탐색함
+    if (used == (1 << n) - 1)
+    {
+        return 0;
+    }
+
+    int& ret = dp[last][used];
+    if (ret != -1)
+    {
+        return ret;
+    }
+
+    ret = 0;
+    for (int next = 0; next < n; ++next)
+    {
+        if (used & (1 << next))
+        {
+            continue;
+        }
+
+        ret = max(ret, overlap[last][next] + Restore(next, used | (1 << next)));
+    }
+    return ret;
+}
+
+string GetStr(int last, int used)
+{
+    // 모두 사용했다면 종료
+    if (used == (1 << n) - 1)
+    {
+        return "";
+    }
+
+    for (int next = 0; next < n; ++next)
+    {
+        if (used & (1 << next))
+        {
+            continue;
+        }
+
+        // next를 사용했을때 가장 많이 겹치는 경우라면 사용함
+        if (Restore(last, used) == overlap[last][next] + Restore(next, used | (1 << next)))
+        {
+            return word[next].substr(overlap[last][next])
+                + GetStr(next, used | (1 << next));
+        }
+    }
+
+    return "";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> n;
+
+        memset(dp, -1, sizeof(dp));
+        memset(overlap, 0, sizeof(overlap));
+
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> word[i];
+        }
+
+        // 같은 문자열, 포함되는 문자열 제거
+        {
+            vector<string> temp;
+
+            for (int i = 0; i < n; ++i)
+            {
+                bool contained = false;
+
+                for (int j = 0; j < n; ++j)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    if (word[j].find(word[i]) != string::npos)
+                    {
+                        if (word[j].size() > word[i].size() || j < i)
+                        {
+                            contained = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!contained)
+                {
+                    temp.push_back(word[i]);
+                }
+            }
+
+            n = temp.size();
+
+            for (int i = 0; i < n; ++i)
+            {
+                word[i] = temp[i];
+            }
+        }
+
+        PreCalc();
+
+        int best = -1;
+        int start = -1;
+
+        for (int i = 0; i < n; ++i)
+        {
+            int len = Restore(i, 1 << i);
+
+            if (len > best)
+            {
+                best = len;
+                start = i;
+            }
+        }
+
+        cout << word[start];
+        cout << GetStr(start, 1 << start) << "\n";
+    }
+}
---
+++ Algorithm/Algospot/DP/웨브바짐.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// e는 지금계란가격, 계란 가격은 m의 배수
+string e, digits;   // digits는 e의 자릿수들을 정렬한것
+int n, m;
+
+// dp[visited][mod][less]
+// visited : 사용한 숫자 집합 (비트연산)
+// mod     : 지금까지 만든수를 m으로 나눈 나머지
+// less    : 지금까지 만든수가 앞에서부터 비교했을때 e보다 이미 작은지
+vector<vector<vector<int>>> dp;
+
+constexpr int MOD = 1000000007;
+
+
+// 이번에 채울 자리 인덱스, 사용한 자릿수들,
+// 지금까지 만든 가격의 m에 대한 나머지, 지금까지 만든 가격이 이미 작으면 1 / 동일하면 0
+int GetPrice(int index, int visited, int mod, int less)
+{
+    if (index == n)
+    {
+        return (less && mod == 0) ? 1 : 0;
+    }
+
+    int& ret = dp[visited][mod][less];
+    if (ret != -1)
+    {
+        return ret;
+    }
+
+    ret = 0;
+    for (int next = 0; next < n; ++next)
+    {
+        // 방문하지 않았다면
+        if ((visited & (1 << next)) == 0)
+        {
+            // 과거 가격이 지금 가격보다 크면 넘김
+            if (!less && e[index] < digits[next])
+            {
+                continue;
+            }
+
+            // 같은 숫자는 한번만 선택하도록
+            // 이전숫자와 지금 숫자가 동일한데, 이전숫자를 사용하지 않았다면
+            // 중복이 될 수 있으므로 사용 X
+            if (next > 0 && digits[next - 1] == digits[next] && (visited & (1 << (next - 1))) == 0)
+            {
+                continue;
+            }
+
+            int next_visited = visited | (1 << next);
+            int next_mod = (mod * 10 + digits[next] - '0') % m;
+            int next_less = less || (e[index] > digits[next]);
+            ret += GetPrice(index + 1, next_visited, next_mod, next_less);
+            ret %= MOD;
+        }
+    }
+    return ret;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> e >> m;
+
+        n = e.size();
+        digits = e;
+        sort(digits.begin(), digits.end());
+
+        dp.assign(1 << n, vector<vector<int>>(m, vector<int>(2, -1)));
+
+        cout << GetPrice(0, 0, 0, 0) << "\n";
+    }
+}
