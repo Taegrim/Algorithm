3e1ddc3923be7f4d4ccf5930e3740196581ee732
Taegrim <aopo3948@gmail.com>
2026-08-18 10:34:48 +0900

    알고스팟 DP 미니맥스
    DP 미니맥스 알고리즘
    내 최선의 선택은 상대방의 최선의 선택을 뒤집거나 빼서 얻을 수 있음

---
+++ Algorithm/Algospot/DP/미니맥스/숫자 게임.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+constexpr int EMPTY = -1e9;
+
+int n;
+vector<vector<int>> dp;
+vector<int> numbers;
+
+// 현재 내 차례에서 최선을 다했을 때 얻을 수 있는 점수 차이의 최댓값
+// 내 점수 - 상대방 점수
+int PlayGame(int left, int right)
+{
+    // 모든 수가 사라지면 종료
+    if (left > right)
+    {
+        return 0;
+    }
+
+    int& ret = dp[left][right];
+    if (ret != EMPTY)
+    {
+        return ret;
+    }
+
+    // 내 최종 점수 차이 = 현재 점수 - 상대방이 얻는 최적의 점수차
+    // 숫자를 가져가는 경우
+    ret = max(
+        numbers[left] - PlayGame(left + 1, right),
+        numbers[right] - PlayGame(left, right - 1)
+    );
+
+    // 숫자를 없애는 경우
+    if (right - left + 1 >= 2)
+    {
+        ret = max(ret, -PlayGame(left + 2, right));
+        ret = max(ret, -PlayGame(left, right - 2));
+    }
+
+    return ret;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> n;
+        numbers.assign(n, 0);
+        dp.assign(n, vector<int>(n, EMPTY));
+
+        for (int& num : numbers)
+        {
+            cin >> num;
+        }
+
+        cout << PlayGame(0, n - 1) << "\n";
+    }
+}
---
+++ Algorithm/Algospot/DP/미니맥스/틱택토.cpp
@@ -0,0 +1,179 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<char>> board;
+vector<int> dp;
+
+bool IsFinished(char turn)
+{
+    // 가로줄 검사
+    for (int i = 0; i < 3; ++i)
+    {
+        bool is_finished = true;
+
+        for (int j = 0; j < 3; ++j)
+        {
+            if (board[i][j] != turn)
+            {
+                is_finished = false;
+                break;
+            }
+        }
+
+        if (is_finished)
+        {
+            return true;
+        }
+    }
+
+    // 세로줄 검사
+    for (int i = 0; i < 3; ++i)
+    {
+        bool is_finished = true;
+
+        for (int j = 0; j < 3; ++j)
+        {
+            if (board[j][i] != turn)
+            {
+                is_finished = false;
+                break;
+            }
+        }
+
+        if (is_finished)
+        {
+            return true;
+        }
+    }
+
+    // 대각선 검사
+    if ((board[0][0] == turn && board[1][1] == turn && board[2][2] == turn)
+        || (board[0][2] == turn && board[1][1] == turn && board[2][0] == turn))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+// 9자리 3진수로 변환
+int GetInteger()
+{
+    int ret = 0;
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            ret *= 3;
+            if (board[i][j] == 'o')
+            {
+                ++ret;
+            }
+            else if (board[i][j] == 'x')
+            {
+                ret += 2;
+            }
+        }
+    }
+    return ret;
+}
+
+int CanWin(char turn)
+{
+    // 마지막에 상대방이 둬서 끝났다면 패배
+    if (IsFinished('o' + 'x' - turn))
+    {
+        return -1;
+    }
+
+    int& ret = dp[GetInteger()];
+    if (ret != -2)
+    {
+        return ret;
+    }
+
+    int val = 2;
+
+    // 상대방의 모든 경우에서 최솟값이 되는 경우를 선택함
+    // 상대방이 -1이면 상대방 패배, 0이면 무승부이기 때문
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            if (board[i][j] == '.')
+            {
+                board[i][j] = turn;
+                val = min(val, CanWin('o' + 'x' - turn));
+                board[i][j] = '.';
+
+                // 상대방이 지는 수를 발견하면 내 승리 확정이므로 바로 종료
+                if (val == -1)
+                {
+                    return ret = -val;
+                }
+            }
+        }
+    }
+
+    // 플레이할 수 없거나, 비기는게 최선이면 비김
+    if (val == 2 || val == 0)
+    {
+        return ret = 0;
+    }
+
+    // 최선의 경우가 상대방의 경우랑 반대
+    return ret = -val;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        board.assign(3, vector<char>(3, 0));
+        dp.assign(20000, -2);
+
+        int x_count{};
+        int o_count{};
+        for (int i = 0; i < 3; ++i)
+        {
+            for (int j = 0; j < 3; ++j)
+            {
+                cin >> board[i][j];
+
+                if (board[i][j] == 'x')
+                {
+                    ++x_count;
+                }
+                else if (board[i][j] == 'o')
+                {
+                    ++o_count;
+                }
+            }
+        }
+
+        char turn = x_count > o_count ? 'o' : 'x';
+
+        int ret = CanWin(turn);
+        if (ret == 0)
+        {
+            cout << "TIE\n";
+        }
+        else if (ret == 1)
+        {
+            cout << turn << "\n";
+        }
+        else
+        {
+            cout << 'o' + 'x' - turn << "\n";
+        }
+    }
+}
