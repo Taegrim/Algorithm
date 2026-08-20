3a7825565ddf3be80545e04c3a8d0da1fa94ec40
Taegrim <aopo3948@gmail.com>
2026-08-20 19:25:58 +0900

    알고스팟 이분탐색 문제 풀이
    실수의 이분탐색은 while(left<right) 방식이 안되므로 일정 횟수 반복해서 처리
    정수의 이분탐색은 left, right중 어느것이 포함하는지 판단 필요함

---
+++ Algorithm/Algospot/이분탐색/남극 기지.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+vector<pair<double, double>> points;
+vector<vector<double>> dist;
+int n;
+
+bool BFS(double mid)
+{
+    vector<bool> visited(n, false);
+    queue<int> q;
+
+    q.push(0);
+    visited[0] = true;
+    int count = 1;
+
+    while (!q.empty())
+    {
+        int current = q.front();
+        q.pop();
+
+        // 현재기지와 다른 기지들의 거리 확인
+        for (int next = 0; next < n; ++next)
+        {
+            // 이미 방문했다면 건너뜀
+            if (visited[next]) continue;
+
+            // 통신이 가능하면 연결
+            if (dist[current][next] <= mid)
+            {
+                visited[next] = true;
+                q.push(next);
+                ++count;
+            }
+        }
+    }
+
+    return count == n;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> n;
+
+        points.assign(n, pair<double, double>());
+        dist.assign(n, vector<double>(n, 0.0));
+
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> points[i].first >> points[i].second;
+        }
+
+        // 기지들간의 거리 계산
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i + 1; j < n; ++j)
+            {
+                if (i == j) continue;
+
+                double x = points[i].first - points[j].first;
+                double y = points[i].second - points[j].second;
+
+                dist[i][j] = dist[j][i] = sqrt(x * x + y * y);
+            }
+        }
+
+        double left = 0;
+        double right = 2000.0;
+
+        for (int i = 0; i < 100; ++i)
+        {
+            double mid = left + (right - left) / 2.0;
+
+            // mid 거리로 모든 기지가 연결되어있는지 확인
+            if (BFS(mid))
+            {
+                // 연결가능하면 줄여보기
+                right = mid;
+            }
+            else
+            {
+                // 불가능하면 늘리기
+                left = mid;
+            }
+        }
+
+        cout << fixed;
+        cout.precision(2);
+        cout << right << "\n";
+    }
+}
---
+++ Algorithm/Algospot/이분탐색/캐나다 여행.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, k;   // n개 도시, k번째 표지판
+
+vector<int> L;  // L[i] : i번째 도시까지의 거리
+vector<int> M;  // M[i] : i번째 도시에서 Xm 떨어진 거리부터 표지판 있음
+vector<int> G;  // G[i] : i번째 도시의 표지판 간격
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> n >> k;
+
+        L.assign(n, 0);
+        M.assign(n, 0);
+        G.assign(n, 0);
+
+        int left = 0;
+        int right = 0;
+
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> L[i] >> M[i] >> G[i];
+            right = max(right, L[i]);
+        }
+
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+
+            long long count = 0;
+
+            // 거리 mid 에서 각 도시별로 마주칠 수 있는 표지판의 수를 구하기
+            for (int i = 0; i < n; ++i)
+            {
+                int start = L[i] - M[i];
+                int last = min(mid, L[i]);  // 도시와 현재 거리 중 짧은것이 마지막
+                
+                // 가능한 마지막 표지판 위치가 시작점보다 멀리 있을 경우에만 가능
+                if (last >= start)
+                {
+                    count += (last - start) / G[i] + 1;
+                }
+            }
+
+            if (k <= count)
+            {
+                // mid 까지 이미 k개 이상이 있음
+                // k번째 표지판은 mid 이하에 있음
+                right = mid;
+            }
+            else
+            {
+                // 아직 k개 미만
+                // k번째 표지판은 mid 뒤에 있음
+                left = mid + 1;
+            }
+        }
+
+        cout << left << "\n";
+    }
+}
