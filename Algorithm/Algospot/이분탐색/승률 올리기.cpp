541955f5345291e0a30c15289ba2151d7202750d
Taegrim <aopo3948@gmail.com>
2026-08-20 22:48:35 +0900

    승률 올리기, 전세금 균등상환
    알고스팟 이분탐색 풀이

---
+++ Algorithm/Algospot/이분탐색/승률 올리기.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long n, m;
+// n : 플레이 횟수
+// m : 승리 횟수
+
+constexpr long long MAX = 200000000LL;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> n >> m;
+
+        int goal = 100 * m / n + 1;
+
+        // 승률이 99퍼거나, 100퍼였다면 1퍼 올리기는 불가능함
+        if (goal >= 100)
+        {
+            cout << -1 << "\n";
+            continue;
+        }
+
+        long long left = 0;
+        long long right = MAX;
+
+        while (left < right)
+        {
+            long long mid = left + (right - left) / 2;
+
+            long long rate = (m + mid) * 100 / (n + mid);
+
+            if (rate >= goal)
+            {
+                // 가능하면 구간 줄이기
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        cout << left << "\n";
+    }
+}
---
+++ Algorithm/Algospot/이분탐색/전세금 균등상환.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+double p;
+// n : 대출금액
+// m : M개월동안 갚음
+// p : 월 (P/12)% 만큼 이자
+
+// c : 한달마다 상환할 금액
+double GetRemaining(int n, int m, double p, double c)
+{
+    double result = n;
+
+    for (int i = 0; i < m; ++i)
+    {
+        result *= (1.0 + (p / 12.0) / 100.0);
+        result -= c;
+
+        if (result <= 0.0)
+        {
+            break;
+        }
+    }
+
+    return result;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> n >> m >> p;
+
+        double left = 0.0;
+        double right = n * (1.0 + (p / 12.0) / 100.0);  // 1달만에 전부 갚기
+
+        for (int i = 0; i < 100; ++i)
+        {
+            double mid = left + (right - left) / 2.0;
+
+            if (GetRemaining(n, m, p, mid) <= 0.0)
+            {
+                // 갚을 수 있다면 금액 줄이기
+                right = mid;
+            }
+            else
+            {
+                // 갚을 수 없다면 금액 늘리기
+                left = mid;
+            }
+        }
+
+        cout << fixed;
+        cout.precision(10);
+        cout << right << "\n";
+    }
+}
