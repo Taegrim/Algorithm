35b7b969ed6109fce73b98bcaa5397d7e4c74b70
Taegrim <aopo3948@gmail.com>
2026-08-21 21:20:38 +0900

    정수론
    정수론 기본 함수 형태 작성
    비밀번호 486 풀이

---
+++ Algorithm/Algospot/정수론/비밀번호 486.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, lo, hi;
+
+int MAX = 10000000;
+vector<int> min_factor(MAX + 1);    // min_factor[i] : i의 가장 작은 소인수
+vector<int> min_factor_power(MAX + 1); // min_factor_power[i] : i의 소인수 분해엔 min_factor가 몇승이 포함되어있는지
+vector<int> factors(MAX + 1); // factos[i] : i가 가진 약수 개수
+
+void Pref()
+{
+    min_factor[0] = min_factor[1] = -1;
+
+    for (int i = 2; i <= MAX; ++i)
+    {
+        min_factor[i] = i;
+    }
+
+    for (int i = 2; i * i <= MAX; ++i)
+    {
+        if (min_factor[i] == i)
+        {
+            for (int j = i * i; j <= MAX; j += i)
+            {
+                if (min_factor[j] == j)
+                {
+                    min_factor[j] = i;
+                }
+            }
+        }
+    }
+
+    factors[1] = 1;
+
+    for (int i = 2; i <= MAX; ++i)
+    {
+        // 소수라면
+        if (min_factor[i] == i)
+        {
+            // 자기자신 1승, 약수 2개
+            min_factor_power[i] = 1;
+            factors[i] = 2;
+        }
+        else
+        {
+            // 소수가 아니면 가장 작은 소인수로 나눈 수의 약수를 이용해 i의 약수 개수 찾음
+            int p = min_factor[i];
+            int remain = i / p;
+
+            // 가장 작은 소인수를 떼어냈을 때 더이상 없다면 1개라고 기록
+            if (p != min_factor[remain])
+            {
+                min_factor_power[i] = 1;
+            }
+            else
+            {
+                // 아직 해당 소인수가 남아있다면 나머지 수에 대한 min_factor_power 에서 1을 더함
+                min_factor_power[i] = min_factor_power[remain] + 1;
+            }
+
+            int a = min_factor_power[i];
+            factors[i] = (factors[remain] / a) * (a + 1);
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    // 전처리
+    Pref();
+
+    while (t--)
+    {
+        cin >> n >> lo >> hi;
+
+        int ret = 0;
+        for (int i = lo; i <= hi; ++i)
+        {
+            if (factors[i] == n)
+            {
+                ++ret;
+            }
+        }
+
+        cout << ret << "\n";
+    }
+}
---
+++ Algorithm/Algospot/정수론/정수론 함수.cpp
@@ -0,0 +1,136 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 소수 판별 기본
+bool IsPrime(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+    if (n <= 3)
+    {
+        return true;
+    }
+    if (n % 2 == 0 || n % 3 == 0)
+    {
+        return false;
+    }
+
+    for (int k = 5; k * k <= n; k += 6)
+    {
+        if (n % k == 0 || n % (k + 2) == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+// 소인수 분해
+vector<int> FactorSimple(int n)
+{
+    vector<int> ret;
+
+    for (int i = 2; i * i <= n; ++i)
+    {
+        while (n % i == 0)
+        {
+            n /= i;
+            ret.push_back(i);
+        }
+    }
+
+    if (n > 1)
+    {
+        ret.push_back(n);
+    }
+
+    return ret;
+}
+
+// 에라토스테네스의 체
+vector<bool> Eratosthenes(int n)
+{
+    vector<bool> numbers(n + 1, true);
+
+    numbers[0] = numbers[1] = false;
+
+    for (int i = 2; i * i <= n; ++i)
+    {
+        // 이 수가 지워지지 않았다면 배수들을 false로 변경
+        if (numbers[i])
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                numbers[j] = false;
+            }
+        }
+    }
+
+    return numbers;
+}
+
+
+// min_factor[i] : i의 가장 작은 소인수, i가 소수면 자기자신
+int MAX_N;
+vector<int> min_factor;
+
+// 에라토스테네스의 체로 소인수 분해 정보 저장
+void Eratosthenes2()
+{
+    min_factor[0] = min_factor[1] = -1;
+
+    // 모든 수에 대해 자기자신으로 설정하고 시작
+    for (int i = 2; i <= MAX_N; ++i)
+    {
+        min_factor[i] = i;
+    }
+
+    for (int i = 2; i * i <= MAX_N; ++i)
+    {
+        if (min_factor[i] == i)
+        {
+            for (int j = i * i; j <= MAX_N; j += i)
+            {
+                // 변경하지 않은 경우에만 약수 변경
+                if (min_factor[j] = j)
+                {
+                    min_factor[j] = i;
+                }
+            }
+        }
+    }
+}
+
+// 에라토스테네스의 체를 이용한 소인수 분해 빠르게 구하기
+vector<int> Factor(int n)
+{
+    vector<int> ret;
+
+    while (n > 1)
+    {
+        ret.push_back(min_factor[n]);
+        n /= min_factor[n];
+    }
+
+    return ret;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    MAX_N = 10000;
+    min_factor.assign(MAX_N + 1, -1);
+
+    Eratosthenes2();
+
+    for (int i : Factor(9991))
+    {
+        cout << i << "\n";
+    }
+}
